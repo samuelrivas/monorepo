ec6b6e4de59ec57d604bc86600dc549a14c729db
Samuel Rivas <samuelrivas@gmail.com>
2016-02-22 14:53:09 +0100

    Lazy update so that we can delete only the local brnaches

--- src/shell/assorted-scripts/src/git-delete-merged.sh
+++ src/shell/assorted-scripts/src/git-delete-merged.sh
@@ -19,7 +19,7 @@ readonly PROGNAME="$(basename "$0")"
 ## Functions
 ##====================================================================
 usage() {
-    echo "Usage ${PROGNAME} <our-remote> <upstream-branch>" >&2
+    echo "Usage ${PROGNAME} <local|all> <our-remote> <upstream-branch>" >&2
 }
 
 usage_and_exit() {
@@ -35,31 +35,50 @@ get_merged() {
         --merged "$upstream_branch"
 }
 
-delete_branch() {
+delete_remote_branch() {
     local long_name="$1"
     local remote="$(echo "$long_name" | cut -d/ -f1)"
     local branch="$(echo "$long_name" | cut -d/ -f2)"
     local sha="$(git rev-parse "$long_name")"
 
-    echo "Deleting $sha ($long_name)"
-    git push "$remote" :"$branch"
-    git branch -D "$branch"
+    echo "Deleting remote $sha ($long_name)"
+    if ! git push "$remote" :"$branch"; then
+        echo "Couldn't delete remote branch: $long_name"
+    fi
+}
+
+delete_local_branch() {
+    local long_name="$1"
+    local branch="$(echo "$long_name" | cut -d/ -f2)"
+    local sha="$(git rev-parse "$long_name")"
+
+    echo "Deleting local $sha ($branch)"
+    if ! git branch -D "$branch"; then
+        echo "Couldn't delete local branch: $branch"
+    fi
 }
 
 ## Main
 ##====================================================================
 main() {
-    (( $NARGS != 2 )) && usage_and_exit
-    local upstream_branch our_remote merged branch
+    (( $NARGS != 3 )) && usage_and_exit
+    local remove_mode upstream_branch our_remote merged branch
 
-    our_remote="${ARGS[0]}"
-    upstream_branch="${ARGS[1]}"
+    remove_mode="${ARGS[0]}"
+    our_remote="${ARGS[1]}"
+    upstream_branch="${ARGS[2]}"
     merged=("$(get_merged "$our_remote" "$upstream_branch")")
+
+    if [[ "$remove_mode" != "local" && "$remove_mode" != "all" ]]; then
+        usage_and_exit
+    fi
 
     for branch in ${merged[@]}; do
         if [[ ! "$branch" =~ "/master" ]]; then
-            delete_branch "$branch"
-            echo $branch
+            delete_local_branch "$branch"
+            if [[ "$remove_mode" == "all" ]]; then
+                delete_remote_branch "$branch"
+            fi
         fi
     done
 }
