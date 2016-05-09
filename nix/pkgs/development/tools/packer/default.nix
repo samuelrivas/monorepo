4b1a1df3193a97114cd43a7b79967e03c80e7386
Samuel Rivas <samuelrivas@gmail.com>
2016-05-09 15:25:40 +0200

    Support moar platforms
    Just so that we can PR this upstream

--- nix/pkgs/development/tools/packer/default.nix
+++ nix/pkgs/development/tools/packer/default.nix
@@ -5,14 +5,28 @@
 
 let
   version = "0.10.1";
+  package-system-names = {
+    x86_64-darwin = "darwin_amd64";
+    x86_64-linux = "linux_amd64";
+    x86_64-freebsd = "freebsd_amd64";
+    x86_64-openbsd = "openbsd_amd64";
+  };
+  package-system-name = builtins.getAttr stdenv.system package-system-names;
 in
+
+# doesn't work without stringification for some reason
+assert package-system-names ? "${stdenv.system}";
+
 stdenv.mkDerivation rec {
   name = "packer-${version}";
+
   src = fetchzip {
     stripRoot = false;
-    url = "https://releases.hashicorp.com/packer/${version}/packer_${version}_linux_amd64.zip";
+    url = "https://releases.hashicorp.com/packer/${version}/packer_${version}_${package-system-name}.zip";
     sha256 = "06yb3pjvk39b3mwkk3wy7lzl9rxngq7b7wjvscbz3f47s6v4vh8m";
   };
+
+  platforms = builtins.attrNames package-system-names;
 
   phases = [ "checkPhase" "installPhase" ];
 
