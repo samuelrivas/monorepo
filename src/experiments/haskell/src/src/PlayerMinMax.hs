df1ca476e319f204da67900111e1e36288ae4737
Samuel Rivas <samuelrivas@gmail.com>
2016-06-16 19:08:50 +0200

    extract nix-shell to a script

--- src/experiments/haskell/src/src/PlayerMinMax.hs
+++ src/experiments/haskell/src/src/PlayerMinMax.hs
@@ -3,11 +3,16 @@ module PlayerMinMax (
   ) where
 
 import qualified Data.Map.Lazy as Map
+import qualified Data.Maybe    as Maybe
 import qualified Data.Set      as Set
 import qualified GameState
 import qualified Player
 
 {-# ANN module "HLint: ignore Use camelCase" #-}
+
+data GameTree move state = GameTree { get_state :: state
+                                    , children  :: [(move, GameTree move state)]
+                                    }
 
 mk_player :: Player.Player
 mk_player = Player.mk_player next_move
@@ -26,3 +31,14 @@ h state =
         in op acc (GameState.player_score state player)
   in
     Set.foldl accumulate 0 GameState.players
+
+game_tree :: GameState.State -> GameTree GameState.Move GameState.State
+game_tree state =
+  let possible_moves = GameState.possible_moves state
+      mk_child move = do
+        s <- GameState.next_state state move
+        return (move, game_tree s)
+  in
+  GameTree { get_state = state
+           , children  = Maybe.mapMaybe mk_child possible_moves
+           }
