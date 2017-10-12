5d53e69afda77c4be69e7f0ead6b4b51602ecfed
Samuel Rivas <samuelrivas@gmail.com>
2017-10-12 11:33:38 +0200

    Use ensime from upstream melpa stable

--- nix/default.nix
+++ nix/default.nix
@@ -89,7 +89,8 @@ let
       (with pkgs; {
         inherit (emacsPackagesNg) flycheck-haskell haskell-mode
                                   nix-mode groovy-mode tuareg
-                                  terraform-mode ensime;
+                                  terraform-mode;
+        inherit (self.upstream-pkgs.emacsPackagesNg.melpaStablePackages) ensime;
         inherit (emacsPackages) scalaMode2 erlangMode colorThemeSolarized;
         inherit (haskellPackages) hlint stylish-haskell;
         inherit (ocamlPackages_4_02) merlin ocpIndent utop;
