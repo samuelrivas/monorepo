8af1ead1e84af9ced67505378d752c36f8da243f
Samuel Rivas <samuelrivas@gmail.com>
2018-02-15 08:37:16 +0100

    Get rid of the first pass

--- src/c++/algos-n-fun/src/src/string_matching_fsm.cpp
+++ src/c++/algos-n-fun/src/src/string_matching_fsm.cpp
@@ -23,32 +23,31 @@ using std::setw;
 /* fsm[i][c] returns the amount of matched characters after reading
  * c, given that the previous amount was i
  *
- * This is linear on the length of query, but could be even "more linear" at the
- * cost of memory if we used an array instead of a map to store the possible
- * characters (which would let us use an array for "seen" too)
+ * This is linear on the length of query (assuming that the radix is irrelevant,
+ * but could be even "more linear" at the cost of memory if we used an array
+ * instead of a map to store the possible characters (which would let us use an
+ * array for "seen" too, but make the radix a lot more relevant)
  */
 vector<unordered_map<char, int>> query_fsm(string query) {
   vector<unordered_map<char, int>> fsm(query.length());
 
-  // first pass setting the forward links
-
-  for (size_t i = 0; i < query.length(); i++) {
-    fsm[i][query[i]] = i + 1;
-  }
-
-  /* second pass setting the backward links
-   *
-   * mismatched_satate is the state of a machine that has matched all but the
+  /* Mismatched_satate is the state of a machine that has matched all but the
    * first character of the pattern
    */
   unordered_set<char> seen { query[0] };
+
   int mismatched_state = 0;
+  fsm[0][query[0]] = 1;
+
   for (size_t i = 1; i < query.length(); i++) {
+
+    // Mismatcthing transitions
     for (char c : seen) {
-      if (fsm[i][c] == 0) {
-        fsm[i][c] = fsm[mismatched_state][c];
-      }
+      fsm[i][c] = fsm[mismatched_state][c];
     }
+
+    // Matching transition
+    fsm[i][query[i]] = i + 1;
     mismatched_state = fsm[mismatched_state][query[i]];
     seen.insert(query[i]);
   }
