ce4afea1c7e21805de39ddb63fb73b33b7cfca76
Samuel Rivas <samuelrivas@gmail.com>
2018-03-28 16:09:44 +0200

    efficient queens

--- src/c++/algos-n-fun/src/src/queens.cpp
+++ src/c++/algos-n-fun/src/src/queens.cpp
@@ -1,15 +1,16 @@
-/* We index the board as a double array where the first dimension is rows, top down, and the second is columns, left to right.
+/* We index the board as a double array where the first dimension is rows, top
+   down, and the second is columns, left to right.
 
-We also index diagonals:
+   We also index diagonals:
 
-Bottom left to top right diagonals are indexed between 0 and 14 (inclusive) such as cell[x][y] is in diagonal x + y.
+   Bottom left to top right diagonals are indexed between 0 and 14 (inclusive)
+   such as cell[x][y] is in diagonal x + y.
 
-Top left to bottom right diagonals are indexed so that cell[x][y] is in diagonal 7 + (x - y) (arbitrarily keeping this in the same index space, though is not really needed) */
-
+   Top left to bottom right diagonals are indexed so that cell[x][y] is in
+   diagonal 7 + (x - y) (so that we can directly map this to an array index)
+ */
 #include <unordered_set>
-#include <set>
 #include <utility>
-#include <cassert>
 #include <iostream>
 #include <vector>
 #include <array>
@@ -19,61 +20,53 @@ using std::pair;
 using std::cout;
 using std::endl;
 using std::vector;
-using std::set;
 using std::array;
 
-typedef array<bool, 8> RowCol;
 typedef array<bool, 15> Diag;
 typedef pair<int, int> Coord;
 
-void queens(set<Coord> positions, RowCol rows, RowCol cols, Diag bltr, Diag tlbr, set<set<Coord>>* solutions) {
-  if (positions.size() == 8) {
-    solutions -> insert(positions);
+void queens(array<Coord, 8> partial, int col, unordered_set<int> avail_rows,
+            Diag avail_bltr, Diag avail_tlbr,
+            vector<array<Coord, 8>>* solutions) {
+
+  if (col == 8) {
+    solutions -> push_back(move(partial));
     return;
   }
 
-  for (int row = 0; row < 8; row++) {
-    for (int col = 0; col < 8; col++) {
-      if (rows[row] && cols[col] && bltr[row + col] && tlbr[7 + row - col]) {
-        set<Coord> new_positions(positions);
-        new_positions.insert(pair<int, int>(row, col));
+  for (int row : avail_rows) {
+    if (avail_bltr[row + col] && avail_tlbr[7 + row - col]) {
+      array<Coord, 8> new_partial = partial;
+      new_partial[col] = pair<int, int>(row, col);
+      unordered_set<int> new_avail_rows(avail_rows);
+      new_avail_rows.erase(row);
+      Diag new_avail_bltr(avail_bltr);
+      new_avail_bltr[row + col] = false;
+      Diag new_avail_tlbr(avail_tlbr);
+      new_avail_tlbr[7 + row - col] = false;
 
-        RowCol new_rows(rows);
-        new_rows[row] = false;
-        RowCol new_cols(cols);
-        new_cols[col] = false;
-
-        Diag new_bltr(bltr);
-        new_bltr[row + col] = false;
-
-        Diag new_tlbr(tlbr);
-        new_tlbr[7 + row - col] = false;
-
-        queens(move(new_positions), move(new_rows), move(new_cols), move(new_bltr), move(new_tlbr), solutions);
-      }
+      queens(move(new_partial), col + 1, move(new_avail_rows),
+             move(new_avail_bltr), move(new_avail_tlbr), solutions);
     }
   }
 }
 
-set<set<Coord>> queens() {
-  set<set<Coord>> solutions;
-  set<Coord> positions;
-  RowCol rows;
-  rows.fill(true);
-  RowCol cols;
-  cols.fill(true);
+vector<array<Coord, 8>> queens() {
+  vector<array<Coord, 8>> solutions;
+  array<Coord, 8> partial;
+  unordered_set<int> rows {0, 1, 2, 3, 4, 5, 6, 7};
   Diag bltr;
   bltr.fill(true);
   Diag tlbr;
   tlbr.fill(true);
 
-  queens(move(positions), move(rows), move(cols), move(bltr), move(tlbr), &solutions);
+  queens(move(partial), 0, move(rows), move(bltr), move(tlbr), &solutions);
   return solutions;
 }
 
 int main(void) {
 
-  for (set<Coord> solution: queens()) {
+  for (array<Coord, 8> solution: queens()) {
     for (Coord coord : solution) {
       cout << "(" << coord.first << "," << coord.second << ") ";
     }
