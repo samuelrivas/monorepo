e30c45fd00eb0e6c524c3ebef32473188d2a745e
Samuel Rivas <samuelrivas@gmail.com>
2018-05-07 15:55:03 +0200

    heavy debugging going on

--- src/c++/algos-n-fun/src/src/adjoin.cpp
+++ src/c++/algos-n-fun/src/src/adjoin.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
+#include <tuple>
 #include <utility>
 
 #include "lib/graph.hpp"
 
 using std::cout;
 using std::endl;
-using std::pair;
+using std::tuple;
+using std::get;
+using std::move;
+using std::get;
 
 class FurthestCb : public BfsCallbacks {
   vector<int> hops;
   int max_hops = -1;
   int furthest = 0;
+  vector<bool> visited;  // Todo this duplicates data from CB
 
  public:
-  FurthestCb(const Graph& _graph) : hops(_graph.n_vertices(), -1) { }
+  FurthestCb(const Graph& _graph) :
+    hops(_graph.n_vertices(), -1),
+    visited(_graph.n_vertices(), false)
+  { }
 
   void on_exit(int vertex, const vector<int>& parent, const vector<State>& state) {
     (void) state;
@@ -28,29 +36,32 @@ class FurthestCb : public BfsCallbacks {
       max_hops = hops[vertex];
       furthest = vertex;
     }
+    visited[vertex] = true;
   }
 
-  int get_furthest() {
+  int get_furthest() const {
     return furthest;
   }
-  int get_distance() {
+  int get_distance() const {
     return max_hops;
+  }
+  vector<bool> get_visited() {
+    return visited;
   }
 };
 
-pair<int, int> furthest_from(const Graph& graph, int vertex) {
+// {furthest, distance, visited}
+tuple<int, int, vector<bool>> furthest_from(const Graph& graph, int vertex) {
   FurthestCb cb(graph);
   Bfs bfs(graph, &cb);
   bfs.bfs(vertex);
 
-  return {cb.get_furthest(), cb.get_distance()};
+  return {cb.get_furthest(), cb.get_distance(), cb.get_visited()};
 }
 
 int main(void) {
   Graph graph(8);
 
-  graph.connect(7, 6);
-  graph.connect(0, 1);
   graph.connect(0, 2);
   graph.connect(5, 1);
   graph.connect(3, 1);
@@ -60,10 +71,10 @@ int main(void) {
   graph.connect(3, 7);
 
 
-  pair<int, int> extreme_a = furthest_from(graph, 0);
-  pair<int, int> extreme_b = furthest_from(graph, extreme_a.first);
+  tuple<int, int, vector<bool>> extreme_a = furthest_from(graph, 0);
+  tuple<int, int, vector<bool>> extreme_b = furthest_from(graph, get<0>(extreme_a));
 
-  int diameter = extreme_b.second;
+  int diameter = get<1>(extreme_b);
 
-  cout << "Diameter: " << diameter << ". From " << extreme_a.first << " to " << extreme_b.first << endl;
+  cout << "Diameter: " << diameter << ". From " << get<0>(extreme_a) << " to " << get<0>(extreme_b) << endl;
 }
