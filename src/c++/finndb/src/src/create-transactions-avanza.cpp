cde18d89d1445a47e107756228286df2bc44fc2e
Samuel Rivas <samuelrivas@gmail.com>
2018-07-15 14:37:18 +0200

    another step towards downloading avanza

--- src/c++/finndb/src/src/create-transactions-avanza.cpp
+++ src/c++/finndb/src/src/create-transactions-avanza.cpp
@@ -1,13 +1,19 @@
 /* Copyright 2017 samuelrivas@gmail.com
  *
- * Add a sha1 at the end of each line, separated with a semicolon (this is
- * hardcoded for now)
+ * The transaction id is the sha1 of the UTF-8 encoding of the full csv line
+ * being translated, without the newline character
+ *
+ * The movement id is the sha1 of the all the original used fields concatenated
+ * using semicolons to separate them. Check the code below for more details
+ * (look for sha1 calls)
  */
 
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <cassert>
+#include <utility>
 #include <boost/format.hpp>
 
 #include "lib/sha1.hpp"
@@ -17,6 +23,8 @@ using std::cerr;
 using std::cin;
 using std::string;
 using std::vector;
+using std::ostringstream;
+using std::pair;
 using boost::format;
 
 vector<string> split(const string& in, char sep) {
@@ -33,20 +41,57 @@ vector<string> split(const string& in, char sep) {
   return out;
 }
 
-// We assume that the cash amount is always xxxx.xx
-string convert_amount(const string& amount) {
+pair<string, string> convert_amount(const string& amount) {
   vector<string> tokens = split(amount, ',');
-  if (tokens.size() != 2 || tokens[1].size() != 2) {
-    cerr << format("'%s' violates the expected amount format\n")
-      % amount;
-    for (string s : tokens) {
-      cout << s << std::endl;
-    }
-    std::flush(cerr);
-    assert(false);
+  if (tokens.size() == 1) {
+    return pair<string, string> { amount, "0" };
+  } else {
+    string decimals = str (format("%d") % tokens[1].size());
+    return pair<string, string> { tokens[0] + tokens[1], decimals };
   }
 
-  return tokens[0] + tokens[1];
+  cerr << format("'%s' violates the expected amount format\n")
+    % amount;
+  std::flush(cerr);
+  assert(false);
+}
+
+string transaction_line(const string& transaction_id,
+                        const string& date,
+                        const string& raw_line) {
+  ostringstream out;
+  out << "INSERT INTO transactions (id, date, raw) VALUES "
+      << format("(\"%s\",\"%s\",\"%s\");\n")
+    % transaction_id
+    % date
+    % raw_line;
+
+  return out.str();
+}
+
+string movement_line(const string& date,
+                     const string& asset,
+                     const string& account,
+                     const string& amount,
+                     const string& transaction_id) {
+  format id_format("%s;%s;%s;%s;%s");
+  string movement_id = sha1(str(id_format % date % asset % account % amount
+                                % transaction_id));
+  pair<string, string> fp_amount = convert_amount(amount);
+  ostringstream out;
+  out << "INSERT INTO movements "
+    "(id, date, asset, account, amount, decimals, trans_id) VALUES "
+
+      << format("(\"%s\",\"%s\",\"%s\",\"%s\",%s,%s,\"%s\");\n")
+    % movement_id
+    % date
+    % asset
+    % account
+    % fp_amount.first
+    % fp_amount.second
+    % transaction_id;
+
+  return out.str();
 }
 
 int main() {
@@ -63,15 +108,20 @@ int main() {
       assert(false);
     }
 
-    cout << "insert into movements"
-         << " (id, date, account, asset, amount, decimals) values "
-         << format("(\"%s\",\"%s\",\"%s\",\"%s\",%s,2);\n")
-      % sha1(line)
-      % tokens[0]
-      % tokens[1]
-      % tokens[8]
-      % convert_amount(tokens[6]);
+    string transaction_id = sha1(line);
 
+    // Transaction
+    cout << transaction_line(transaction_id, tokens[0], line);
+
+    // Cash movement
+    cout << movement_line(tokens[0], tokens[8], tokens[1],
+                          tokens[6], transaction_id);
+
+    if (tokens[2] == "Köp" || tokens[2] == "Sälj") {
+      // Asset movement
+      cout << movement_line(tokens[0], tokens[3], tokens[1], tokens[4],
+                            transaction_id);
+    }
   }
   return 0;
 }
