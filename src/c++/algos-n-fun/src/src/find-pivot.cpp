938a180b26c9bbfde8b294b994920be2e6decc8a
Samuel Rivas <samuelrivas@gmail.com>
2018-10-06 23:13:21 +0200

    Don't assume that the numbers are positive

--- src/c++/algos-n-fun/src/src/find-pivot.cpp
+++ src/c++/algos-n-fun/src/src/find-pivot.cpp
@@ -9,6 +9,7 @@
 
 using std::vector;
 using std::cout;
+using std::cerr;
 using std::endl;
 using std::setw;
 using std::accumulate;
@@ -20,22 +21,20 @@ int find_pivot(const vector<int>& l) {
 
   int left = 0;
   int right = sum - l[0];
-  size_t pos = 0;
+  int best_pivot = 0;
+  int best_diff = abs(right);
 
-  while (left < right && pos < l.size()) {
-    left += l[pos];
-    pos++;
-    right -= l[pos];
+  for (size_t pivot = 1; pivot < l.size(); pivot++) {
+    left += l[pivot - 1];
+    right -= l[pivot];
+
+    int diff = abs(left - right);
+    if (diff < best_diff) {
+      best_diff = diff;
+      best_pivot = pivot;
+    }
   }
-
-  if (pos == 0) {
-    return 0;
-  }
-
-  int diff_now = abs(left - right);
-  int diff_before = abs(left - l[pos - 1] - (right + l[pos]));
-
-  return (diff_now < diff_before) ? pos : pos - 1;
+  return best_pivot;
 }
 
 int main(void) {
@@ -43,12 +42,13 @@ int main(void) {
     {1, 2, 3},
     {4, 1, 1},
     {1, 1, 4},
-    {10, 3, 7, 2, 9, 4, 1, 1, 3}
+    {10, 3, 7, 2, 9, 4, 1, 1, 3},
+    {10, 9, 0, -2, -3, -8, 3, -5, -2, 9, 3, -1, -2, 2, 7}
   };
 
   for (auto test : tests) {
     for (int n : test) {
-      cout << setw(2) << n;
+      cout << setw(3) << n;
     }
     cout << " : " << find_pivot(test) << endl;
   }
