4372fa7d0ec10e7e55031eb97f792b265d5f04bd
Samuel Rivas <samuelrivas@gmail.com>
2018-10-11 23:06:25 +0200

    better permutations generator

--- src/c++/algos-n-fun/src/src/permutation-generator.cpp
+++ src/c++/algos-n-fun/src/src/permutation-generator.cpp
@@ -21,6 +21,7 @@ using std::string;
 using std::ostringstream;
 using std::max;
 using std::set;
+using std::swap;
 
 int factorial(int n) {
   int result = 1;
@@ -30,9 +31,21 @@ int factorial(int n) {
   return result;
 }
 
+// Permutations are a vector with the most significant digit in position 0. The
+// generator figures out the next permutation in numeric order. The relevant
+// invariants are:
+//
+// - The position to increase is never the last one
+// - Everything to the right of the position to increase is in descending order
+// - We always increase by the minimum possible from the elements to the right
+// - After increasing, everything to the right of the increased position is in
+//   ascending order
+//
+// Just generate the sequence yourself to understand why those rules apply, and
+// why we can just swap the elements when increasing and then reverse the right
+// side to fulfill the last invariant
 class Permutations {
   vector<int> current;
-  int left;
   int elements;
   bool first;
 
@@ -41,63 +54,64 @@ public:
     current(_elements),
     elements { _elements },
     first { true } {
-
-    assert(_elements > 0);
+    assert(_elements > 1);
 
     for (int i = 0; i < elements; i++) {
-      current[i] = elements - i;
-      left = factorial(elements);
+      current[i] = i + 1;
     }
   }
   vector<int> next() {
-    assert(left > 0);
-    left--;
-
-    // UGh
+    // The first time we don't need to generate a new permutation
     if (first) {
       first = false;
       return current;
     }
 
-    set<int> seen;
-    seen.insert(current[0]);
+    int pos_to_change = elements - 2;
 
-    int pos_to_change = 1;
-    int max_seen = current[0];
-    while (max_seen <= current[pos_to_change]) {
-      assert(pos_to_change < elements);
-
-      seen.insert(current[pos_to_change]);
-      max_seen = max(max_seen, current[pos_to_change]);
-      pos_to_change++;
+    // Find a position where we can increase the value
+    while (pos_to_change >= 0
+           && current[pos_to_change] > current[pos_to_change + 1]) {
+      pos_to_change--;
     }
-    cerr << "found at " << pos_to_change << ":" << current[pos_to_change]
-         << endl;
 
-    int to_insert = *(seen.lower_bound(current[pos_to_change]));
-    cerr << "going to insert " << to_insert
-         << endl;
-
-    seen.insert(current[pos_to_change]);
-    seen.erase(to_insert);
-    current[pos_to_change] = to_insert;
-
-    assert(seen.size() == static_cast<size_t>(pos_to_change));
-    int pos_to_insert = pos_to_change - 1;
-    for (int i : seen) {
-      current[pos_to_insert] = i;
-      pos_to_insert--;
+    if (pos_to_change == 0
+        && current[pos_to_change] > current[pos_to_change + 1]) {
+      // Out of permutations
+      assert(false);
     }
+
+        // Find the smallest value to the right that is greater than pos_to_change
+    int pos_to_swap = pos_to_change + 1;
+
+    while (pos_to_swap < elements - 1
+           && current[pos_to_swap + 1] > current[pos_to_change]) {
+      pos_to_swap++;
+    }
+
+    // Swap them
+    swap(current[pos_to_change], current[pos_to_swap]);
+
+    // reverse all that is to the side of pos_to_change
+    for (size_t i = pos_to_change + 1, j = elements - 1; i < j; i++, j--) {
+      swap(current[i], current[j]);
+    }
+
     return current;
   }
 
   bool has_next() {
-    return left > 0;
+    for (size_t i = 1; i < current.size(); i++) {
+      if (current[i - 1] < current[i]) {
+        return true;
+      }
+    }
+    return false;
   }
 };
 
 int main(void) {
-  Permutations perms(4);
+  Permutations perms(6);
 
   while (perms.has_next()) {
     vector<int> perm = perms.next();
