f97e2665a918e855f18dfbdefd883db22d64c309
Samuel Rivas <samuelrivas@gmail.com>
2018-10-17 14:20:52 +0200

    Update convox again

--- nix/pkgs/development/tools/convox/default.nix
+++ nix/pkgs/development/tools/convox/default.nix
@@ -1,8 +1,4 @@
 # Get the binary from convox and fix the interpreter path
-#
-# I expect the convox, but I haven't found a versioned link. Convox
-# documentation asserts that you should install whatever is in that url, on
-# every build ...
 {
   fetchurl,
   stdenv,
@@ -10,18 +6,24 @@
 
 stdenv.mkDerivation rec {
   name = "convox";
-  src = fetchurl {
-    url = "https://convox.com/cli/linux/convox";
-    sha256 = "1191jr796hykrvyg8wggl2ddxqlqnphjafh1a0nxsvqbaybiipl9";
+  convox-binary = fetchurl {
+    # We used to fetchzip this before as source
+    #
+    # url = "https://bin.equinox.io/c/jewmwFCp7w9/convox-stable-linux-amd64.tgz";
+    #
+    # But it is outdated (lots of 404 when we use it) There isn't any other
+    # reliable url to download this thing, so we go ahead and get this unhashed,
+    # unversioned binary and hope for the best
+    url = https://convox.com/cli/linux/convox;
+    sha256 = "1s16hgrnzmjgwxla43ys0cjzqal745hyizm634phjd9s8xrdq1bd";
   };
 
   phases = [ "installPhase" "fixupPhase" ];
 
   installPhase = ''
     mkdir -p "$out/bin"
-    install -v -m755 "$src" "$out/bin/convox"
+    install -v -m755 "${convox-binary}" "$out/bin/convox"
     patchelf --set-interpreter $(cat ${stdenv.cc}/nix-support/dynamic-linker) \
         $out/bin/convox
   '';
 }
-  
