f1e2610021bc1e67be388a97dfb8f53286fb41d5
Samuel Rivas <samuelrivas@gmail.com>
2019-03-05 22:28:57 +0100

    types using GADTs

--- src/haskell/hashcode-photoalbum/src/src/Main.hs
+++ src/haskell/hashcode-photoalbum/src/src/Main.hs
@@ -1,35 +1,55 @@
+{-# LANGUAGE GADTs              #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 import qualified Data.Set       as Set
 import qualified Data.Text.Lazy as T
+
+{-# ANN module "HLint: ignore Use camelCase" #-}
 
 data Picture_info = Picture_info {
   id   :: Int,
   tags :: Set.Set T.Text
-  } deriving (Eq, Ord)
+  } deriving (Eq, Ord, Show)
 
-data Picture = H Picture_info | V Picture_info
-  deriving (Eq, Ord)
+data V = V deriving (Eq, Ord, Show)
+data H = H deriving (Eq, Ord, Show)
 
-make_tags :: [String] -> Set.Set T.Text
-make_tags = Set.fromList . map T.pack
+data Picture a where
+  Picture_v :: Picture_info -> Picture V
+  Picture_h :: Picture_info -> Picture H
+deriving instance Show a => Show (Picture a)
+deriving instance Ord a => Ord (Picture a)
+deriving instance Eq a => Eq (Picture a)
 
-example :: Set.Set Picture
+data Slide a where
+  Slide_v :: Picture V -> Picture V -> Slide V
+  Slide_h :: Picture H -> Slide H
+deriving instance Show a => Show (Slide a)
+deriving instance Ord a => Ord (Slide a)
+deriving instance Eq a => Eq (Slide a)
+
+mk_tags :: [String] -> Set.Set T.Text
+mk_tags = Set.fromList . map T.pack
+
+example :: Set.Set (Either (Picture H) (Picture V))
 example = Set.fromList [
-  H Picture_info {
+  Left $ Picture_h Picture_info {
       Main.id = 1,
-      tags = Set.fromList $ T.pack <$> ["cat", "beach", "sun"]
+      tags = mk_tags ["cat", "beach", "sun"]
       },
-  V Picture_info {
+  Right $ Picture_v Picture_info {
       Main.id = 2,
-      tags = Set.fromList $ T.pack <$> ["selfie", "smile"]
+      tags = mk_tags ["selfie", "smile"]
       },
-  V Picture_info {
+  Right $ Picture_v Picture_info {
       Main.id = 3,
-      tags = make_tags ["garden", "selfie"]
+      tags = mk_tags ["garden", "selfie"]
       },
-  H Picture_info {
+  Left $ Picture_h Picture_info {
       Main.id = 4,
-      tags = make_tags ["garden", "cat"]
+      tags = mk_tags ["garden", "cat"]
       }
   ]
+
 main :: IO ()
 main =  putStrLn "Hello World"
