9620ae5bc65b6badb2007c361257c7ec7c79eb26
Samuel Rivas <samuelrivas@gmail.com>
2019-03-09 21:59:39 +0100

    scoring function

--- src/haskell/hashcode-photoalbum/src/src/Main.hs
+++ src/haskell/hashcode-photoalbum/src/src/Main.hs
@@ -26,8 +26,12 @@ mk_h_picture = H . Picture_h
 mk_tags :: [String] -> Set.Set T.Text
 mk_tags = Set.fromList . map T.pack
 
-example :: Set.Set Picture
-example = Set.fromList [
+get_info :: Picture -> Picture_info
+get_info (V (Picture_v info)) = info
+get_info (H (Picture_h info)) = info
+
+example :: [Picture]
+example = [
    mk_h_picture Picture_info {
       Main.id = 1,
       tags = mk_tags ["cat", "beach", "sun"]
@@ -61,6 +65,39 @@ example = Set.fromList [
       tags = mk_tags ["mountain", "nature", "bird"]
       }
   ]
+
+slide_example_1 :: Slide
+slide_example_1 = Slide_h $ Picture_h
+   Picture_info {
+      Main.id = 1,
+      tags = mk_tags ["cat", "dog", "beach", "sun"]
+      }
+
+slide_example_2 = Slide_v
+  (Picture_v Picture_info {
+    Main.id = 1,
+    tags = mk_tags ["beach", "sun", "parrots"]
+  })
+  (Picture_v Picture_info {
+  Main.id = 1,
+  tags = mk_tags ["sun", "jiraffes"]
+  })
+
+get_tags :: Slide -> Set.Set T.Text
+get_tags (Slide_v p1 p2) =
+  let extract_tags (Picture_v info) = tags info
+  in Set.union (extract_tags p1) (extract_tags p2)
+get_tags (Slide_h (Picture_h p)) =
+  tags p
+
+interest_factor :: Slide -> Slide -> Int
+interest_factor s1 s2 = let
+  t1 = get_tags s1
+  t2 = get_tags s2
+  in
+  minimum $ Set.size <$> [Set.intersection t1 t2,
+                          Set.difference t1 t2,
+                          Set.difference t2 t1]
 
 main :: IO ()
 main =  putStrLn "Hello World"
