097e8700878b2d07506e2a0a0e84974a74e101d4
Samuel Rivas <samuelrivas@gmail.com>
2019-03-10 22:31:05 +0100

    first greedy function

--- src/haskell/hashcode-photoalbum/src/src/Main.hs
+++ src/haskell/hashcode-photoalbum/src/src/Main.hs
@@ -12,6 +12,7 @@ data Picture = Picture {
   } deriving (Eq, Ord, Show)
 
 type Slide = [Picture]
+type Tags = Set.Set T.Text
 
 mk_tags :: [String] -> Set.Set T.Text
 mk_tags = Set.fromList . map T.pack
@@ -39,22 +40,22 @@ example = [
       orientation = V
       },
   Picture {
-      Main.id = 4,
+      Main.id = 5,
       tags = mk_tags ["moutain", "bird"],
       orientation = V
       },
   Picture {
-      Main.id = 4,
+      Main.id = 6,
       tags = mk_tags ["garden", "cat"],
       orientation = H
       },
   Picture {
-      Main.id = 4,
+      Main.id = 7,
       tags = mk_tags ["mountain", "nature"],
       orientation = H
       },
   Picture {
-      Main.id = 4,
+      Main.id = 8,
       tags = mk_tags ["mountain", "nature", "bird"],
       orientation = H
       }
@@ -63,15 +64,12 @@ example = [
 get_tags :: Slide -> Set.Set T.Text
 get_tags = foldl (flip $ Set.union . tags) Set.empty
 
-interest_factor :: Slide -> Slide -> Int
-interest_factor s1 s2 = let
-  t1 = get_tags s1
-  t2 = get_tags s2
-  subsets = ($ t2) . ($ t1) <$> [Set.intersection,
-                                 Set.difference,
-                                 flip Set.difference]
-  in
-  minimum $ Set.size <$> subsets
+interest_factor :: Tags -> Tags -> Int
+interest_factor t1 t2 =
+  let subsets = ($ t2) . ($ t1) <$> [Set.intersection,
+                                     Set.difference,
+                                     flip Set.difference]
+  in minimum $ Set.size <$> subsets
 
 -- find_best :: Foldable t => Slide -> t Picture_h -> Picture
 -- find_best s ps =
@@ -85,6 +83,18 @@ interest_factor s1 s2 = let
 --       best_slide = snd $ foldl f (0, s) ps
 --   in
 --     H $ get_picture best_slide
+
+find_next :: Foldable t => Slide -> t Picture -> Picture
+find_next slide = let
+  origin_tags = get_tags slide
+  f acc@(best_interest, _) candidate_picture =
+    let candidate_interest =
+          interest_factor origin_tags (tags candidate_picture)
+
+    in if candidate_interest > best_interest
+       then (candidate_interest, candidate_picture)
+       else acc
+  in snd . foldl f (0, head slide)
 
 main :: IO ()
 main =  putStrLn "Hello World"
