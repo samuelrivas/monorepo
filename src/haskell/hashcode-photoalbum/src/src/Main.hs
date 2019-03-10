3dd96ae2bae832ef54d9b850e706508aca62ee5f
Samuel Rivas <samuelrivas@gmail.com>
2019-03-10 22:09:38 +0100

    simplify types

--- src/haskell/hashcode-photoalbum/src/src/Main.hs
+++ src/haskell/hashcode-photoalbum/src/src/Main.hs
@@ -3,101 +3,88 @@ import qualified Data.Text.Lazy as T
 
 {-# ANN module "HLint: ignore Use camelCase" #-}
 
-data Picture_info = Picture_info {
-  id   :: Int,
-  tags :: Set.Set T.Text
+data Orientation = H | V deriving (Eq, Ord, Show)
+
+data Picture = Picture {
+  id          :: Int,
+  tags        :: Set.Set T.Text,
+  orientation :: Orientation
   } deriving (Eq, Ord, Show)
 
-newtype Picture_v = Picture_v Picture_info deriving (Eq, Ord, Show)
-newtype Picture_h = Picture_h Picture_info deriving (Eq, Ord, Show)
-
-data Picture = V Picture_v | H Picture_h deriving (Eq, Ord, Show)
-
-data Slide = Slide_v Picture_v Picture_v
-  | Slide_h Picture_h
-  deriving (Eq, Ord, Show)
-
-mk_v_picture :: Picture_info -> Picture
-mk_v_picture = V . Picture_v
-
-mk_h_picture :: Picture_info -> Picture
-mk_h_picture = H . Picture_h
+type Slide = [Picture]
 
 mk_tags :: [String] -> Set.Set T.Text
 mk_tags = Set.fromList . map T.pack
 
-get_info :: Picture -> Picture_info
-get_info (V (Picture_v info)) = info
-get_info (H (Picture_h info)) = info
-
 example :: [Picture]
 example = [
-   mk_h_picture Picture_info {
+   Picture {
       Main.id = 1,
-      tags = mk_tags ["cat", "beach", "sun"]
+      tags = mk_tags ["cat", "beach", "sun"],
+      orientation = H
       },
-  mk_v_picture Picture_info {
+   Picture {
       Main.id = 2,
-      tags = mk_tags ["selfie", "smile"]
+      tags = mk_tags ["selfie", "smile"],
+      orientation = V
       },
-  mk_v_picture Picture_info {
+  Picture {
       Main.id = 3,
-      tags = mk_tags ["garden", "selfie"]
+      tags = mk_tags ["garden", "selfie"],
+      orientation = V
       },
-  mk_v_picture Picture_info {
+  Picture {
       Main.id = 4,
-      tags = mk_tags ["nature", "vacation"]
+      tags = mk_tags ["nature", "vacation"],
+      orientation = V
       },
-  mk_v_picture Picture_info {
+  Picture {
       Main.id = 4,
-      tags = mk_tags ["moutain", "bird"]
+      tags = mk_tags ["moutain", "bird"],
+      orientation = V
       },
-  mk_h_picture Picture_info {
+  Picture {
       Main.id = 4,
-      tags = mk_tags ["garden", "cat"]
+      tags = mk_tags ["garden", "cat"],
+      orientation = H
       },
-  mk_h_picture Picture_info {
+  Picture {
       Main.id = 4,
-      tags = mk_tags ["mountain", "nature"]
+      tags = mk_tags ["mountain", "nature"],
+      orientation = H
       },
-  mk_h_picture Picture_info {
+  Picture {
       Main.id = 4,
-      tags = mk_tags ["mountain", "nature", "bird"]
+      tags = mk_tags ["mountain", "nature", "bird"],
+      orientation = H
       }
   ]
 
-slide_example_1 :: Slide
-slide_example_1 = Slide_h $ Picture_h
-   Picture_info {
-      Main.id = 1,
-      tags = mk_tags ["cat", "dog", "beach", "sun"]
-      }
-
-slide_example_2 = Slide_v
-  (Picture_v Picture_info {
-    Main.id = 1,
-    tags = mk_tags ["beach", "sun", "parrots"]
-  })
-  (Picture_v Picture_info {
-  Main.id = 1,
-  tags = mk_tags ["sun", "jiraffes"]
-  })
-
 get_tags :: Slide -> Set.Set T.Text
-get_tags (Slide_v p1 p2) =
-  let extract_tags (Picture_v info) = tags info
-  in Set.union (extract_tags p1) (extract_tags p2)
-get_tags (Slide_h (Picture_h p)) =
-  tags p
+get_tags = foldl (flip $ Set.union . tags) Set.empty
 
 interest_factor :: Slide -> Slide -> Int
 interest_factor s1 s2 = let
   t1 = get_tags s1
   t2 = get_tags s2
+  subsets = ($ t2) . ($ t1) <$> [Set.intersection,
+                                 Set.difference,
+                                 flip Set.difference]
   in
-  minimum $ Set.size <$> [Set.intersection t1 t2,
-                          Set.difference t1 t2,
-                          Set.difference t2 t1]
+  minimum $ Set.size <$> subsets
+
+-- find_best :: Foldable t => Slide -> t Picture_h -> Picture
+-- find_best s ps =
+--   let f acc@(best_score, _) p =
+--         let candidate_slide = Slide_h p
+--             score = interest_factor s candidate_slide
+--         in
+--         if score > best_score
+--         then (score, candidate_slide)
+--         else acc
+--       best_slide = snd $ foldl f (0, s) ps
+--   in
+--     H $ get_picture best_slide
 
 main :: IO ()
 main =  putStrLn "Hello World"
