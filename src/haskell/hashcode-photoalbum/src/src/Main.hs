269412200e24f13fecb4184a9dc5788cdf49fb8c
Samuel Rivas <samuelrivas@gmail.com>
2019-03-16 22:26:36 +0100

    Create slide deck ignoring orientation
    Using a naive greedy algorithm

--- src/haskell/hashcode-photoalbum/src/src/Main.hs
+++ src/haskell/hashcode-photoalbum/src/src/Main.hs
@@ -1,3 +1,4 @@
+import qualified Data.Maybe     as Maybe
 import qualified Data.Set       as Set
 import qualified Data.Text.Lazy as T
 
@@ -7,7 +8,7 @@ data Orientation = H | V deriving (Eq, Ord, Show)
 
 data Picture = Picture {
   id          :: Int,
-  tags        :: Set.Set T.Text,
+  tag_list    :: Set.Set T.Text,
   orientation :: Orientation
   } deriving (Eq, Ord, Show)
 
@@ -17,52 +18,52 @@ type Tags = Set.Set T.Text
 mk_tags :: [String] -> Set.Set T.Text
 mk_tags = Set.fromList . map T.pack
 
-example :: [Picture]
-example = [
+example :: Set.Set Picture
+example = Set.fromList [
    Picture {
       Main.id = 1,
-      tags = mk_tags ["cat", "beach", "sun"],
+      tag_list = mk_tags ["cat", "beach", "sun"],
       orientation = H
       },
    Picture {
       Main.id = 2,
-      tags = mk_tags ["selfie", "smile"],
+      tag_list = mk_tags ["selfie", "smile"],
       orientation = V
       },
   Picture {
       Main.id = 3,
-      tags = mk_tags ["garden", "selfie"],
+      tag_list = mk_tags ["garden", "selfie"],
       orientation = V
       },
   Picture {
       Main.id = 4,
-      tags = mk_tags ["nature", "vacation"],
+      tag_list = mk_tags ["nature", "vacation"],
       orientation = V
       },
   Picture {
       Main.id = 5,
-      tags = mk_tags ["moutain", "bird"],
+      tag_list = mk_tags ["moutain", "bird", "nature"],
       orientation = V
       },
   Picture {
       Main.id = 6,
-      tags = mk_tags ["garden", "cat"],
+      tag_list = mk_tags ["garden", "cat", "vacation", "sun"],
       orientation = H
       },
   Picture {
       Main.id = 7,
-      tags = mk_tags ["mountain", "nature"],
+      tag_list = mk_tags ["mountain", "nature", "sun", "selfie"],
       orientation = H
       },
   Picture {
       Main.id = 8,
-      tags = mk_tags ["mountain", "nature", "bird"],
+      tag_list = mk_tags ["mountain", "nature", "bird", "sun"],
       orientation = H
       }
   ]
 
 get_tags :: Slide -> Set.Set T.Text
-get_tags = foldl (flip $ Set.union . tags) Set.empty
+get_tags = foldl (flip $ Set.union . tag_list) Set.empty
 
 interest_factor :: Tags -> Tags -> Int
 interest_factor t1 t2 =
@@ -71,17 +72,39 @@ interest_factor t1 t2 =
         <$> [Set.intersection, Set.difference, flip Set.difference]
   in minimum $ Set.size <$> subsets
 
-find_next :: Foldable t => Slide -> t Picture -> Picture
-find_next slide = let
-  origin_tags = get_tags slide
+find_next :: Foldable t => Tags -> t Picture -> Maybe Picture
+find_next origin_tags = let
   f acc@(best_interest, _) candidate_picture =
     let candidate_interest =
-          interest_factor origin_tags (tags candidate_picture)
+          interest_factor origin_tags (tag_list candidate_picture)
 
     in if candidate_interest > best_interest
-       then (candidate_interest, candidate_picture)
+       then (candidate_interest, Just candidate_picture)
        else acc
-  in snd . foldl f (0, head slide)
+  in snd . foldl f (-1, Nothing)
+
+get_next :: Tags -> Set.Set Picture -> (Maybe Picture, Set.Set Picture)
+get_next tags pictures =
+  let
+    next_picture = find_next tags pictures
+    new_pictures = maybe pictures (`Set.delete` pictures) next_picture
+  in
+    (next_picture, new_pictures)
+
+make_slideshow :: Set.Set Picture -> [[Picture]]
+make_slideshow pictures =
+  make_slideshow_rec (mk_tags []) pictures []
+
+make_slideshow_rec :: Tags -> Set.Set Picture -> [[Picture]] -> [[Picture]]
+make_slideshow_rec latest_tags pictures slideshow =
+  if Set.null pictures
+  then reverse slideshow
+  else
+    let (maybe_next_picture, next_pictures) = get_next latest_tags pictures
+        next_picture = Maybe.fromJust maybe_next_picture
+        next_tags = tag_list next_picture
+    in
+      make_slideshow_rec next_tags next_pictures ([next_picture]:slideshow)
 
 main :: IO ()
 main =  putStrLn "Hello World"
