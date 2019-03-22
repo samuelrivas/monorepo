ee4d69e4f1e5bf59747c92d7e79aa6cf0ccf67f7
Samuel Rivas <samuelrivas@gmail.com>
2019-03-22 15:07:56 +0100

    some more things to make bazel work

--- nix/pkgs/development/tools/bazel-sandbox/default.nix
+++ nix/pkgs/development/tools/bazel-sandbox/default.nix
@@ -1,6 +1,8 @@
 {
   bazel,
+  docker,
   empty-builder,
+  python,
   stdenv,
 }:
 stdenv.mkDerivation {
@@ -8,6 +10,12 @@ stdenv.mkDerivation {
   name = "bazel-sandbox";
   buildInputs = [
     bazel
+    docker
+    python
   ];
+
+  # Hack: Wheel doesn't like ancient timestamps
+  SOURCE_DATE_EPOCH = "1471222800";
+
   builder = empty-builder;
 }
