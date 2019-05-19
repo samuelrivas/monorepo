b6a34c936fc0ffaa0108eeac2dc753699ac2364f
Samuel Rivas <samuelrivas@gmail.com>
2019-05-19 22:22:01 +0200

    This is just to make some examples from the book compile

---
+++ src/haskell/concurrent-haskell/nix/default.nix
@@ -0,0 +1,39 @@
+{
+  emacs,
+  empty-builder,
+  haskellPackages,
+  sandbox,
+  stdenv,
+}:
+let
+  wanted-packages = with haskellPackages; [
+    parallel
+    abstract-par
+    accelerate
+    distributed-process
+    distributed-process-simplelocalnet
+    http-conduit
+    monad-par
+    normaldistribution
+    repa
+    utf8-string
+    xml
+  ];
+  haskell-packages-selector = pkgs: wanted-packages;
+  ghc = haskellPackages.ghcWithPackages haskell-packages-selector;
+in
+stdenv.mkDerivation rec {
+
+  name = "concurrent-haskell";
+  src = ./../src;
+
+  buildInputs = [
+    ghc
+    haskellPackages.hlint
+  ] ++ (if sandbox then [(emacs.override { inherit ghc; })] else []);
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ../build/bin/* $out/bin
+  '';
+}
