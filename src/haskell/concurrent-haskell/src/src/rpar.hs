84cc76f62d3c32bfa6d13b7d54d4feb70acf7e81
Samuel Rivas <samuelrivas@gmail.com>
2019-06-26 22:07:04 +0200

    experiment in the eval monad

--- src/haskell/concurrent-haskell/src/src/rpar.hs
+++ src/haskell/concurrent-haskell/src/src/rpar.hs
@@ -1,17 +1,31 @@
+import           Control.Exception           (evaluate)
 import           Control.Parallel.Strategies (Eval, rpar, rseq, runEval)
+import           Data.Time.Clock             (diffUTCTime, getCurrentTime)
 import           System.Environment          (getArgs)
+
+{-# ANN module "HLint: ignore Use camelCase" #-}
 
 main :: IO ()
 main = do
   [n] <- getArgs
-  let (_, _) = runEval $ [test1, test2, test3, test4] !! (read n - 1)
-  return ()
+  t0 <- getCurrentTime
+  let
+    diffTime = flip diffUTCTime t0
+    test = runEval $ [test1, test2, test3, test4, test5] !! (read n - 1)
+  r <- evaluate test
+  t1 <- getCurrentTime
+  print r
+  t2 <- getCurrentTime
+  let elapsed_1 = diffTime t1
+      elapsed_2 = diffTime t2
+  print elapsed_1
+  print elapsed_2
 
-ffib :: Integer -> Integer
-ffib = (fibs !!) . fromInteger
+small_number :: Integer
+small_number = 36
 
-fibs :: [Integer]
-fibs = 0 : 1 : zipWith (+) (tail fibs) fibs
+large_number :: Integer
+large_number = 38
 
 fib :: Integer -> Integer
 fib 0 = 0
@@ -20,24 +34,31 @@ fib n = fib (n - 1) + fib (n - 2)
 
 test1 :: Eval (Integer, Integer)
 test1 = do
-  a <- rpar $ fib 30
-  b <- rpar $ fib 31
+  a <- rpar $ fib large_number
+  b <- rpar $ fib small_number
   return (a, b)
 
 test2 :: Eval (Integer, Integer)
 test2 = do
-  a <- rpar $ fib 30
-  b <- rpar $ fib 31
+  a <- rpar $ fib large_number
+  b <- rseq $ fib small_number
   return (a, b)
 
 test3 :: Eval (Integer, Integer)
 test3 = do
-  a <- rpar $ fib 30
-  b <- rpar $ fib 31
+  a <- rpar $ fib large_number
+  b <- rseq $ fib small_number
+  _ <- rseq a
   return (a, b)
 
 test4 :: Eval (Integer, Integer)
 test4 = do
-  a <- rpar $ fib 30
-  b <- rpar $ fib 31
+  a <- rseq $ fib large_number
+  b <- rseq $ fib small_number
+  return (a, b)
+
+test5 :: Eval (Integer, Integer)
+test5 = do
+  a <- rseq $ fib large_number
+  b <- rpar $ fib small_number
   return (a, b)
