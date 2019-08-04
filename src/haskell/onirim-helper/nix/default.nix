13bdea4f46939d60ecfe71c193fca4b9d547afab
Samuel Rivas <samuelrivas@gmail.com>
2019-08-04 19:07:11 +0200

    first commit

---
+++ src/haskell/onirim-helper/nix/default.nix
@@ -0,0 +1,26 @@
+{
+  emacs-for-haskell,
+  haskellPackages,
+  sandbox,
+  stdenv,
+}:
+let
+  wanted-packages = with haskellPackages; [
+  ];
+  haskell-packages-selector = pkgs: wanted-packages;
+  ghc = haskellPackages.ghcWithPackages haskell-packages-selector;
+in
+stdenv.mkDerivation rec {
+
+  name = "onirim-helper";
+  src = ./../src;
+
+  buildInputs = [
+    ghc
+  ] ++ (if sandbox then [(emacs-for-haskell ghc)] else []);
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ../build/bin/* $out/bin
+  '';
+}
