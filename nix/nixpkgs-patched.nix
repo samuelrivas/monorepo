9ac744e2a1da9bc9815b767bc11f28532cefd85b
Samuel Rivas <samuelrivas@gmail.com>
2019-11-17 22:22:44 +0100

    ups, typo

--- nix/nixpkgs-patched.nix
+++ nix/nixpkgs-patched.nix
@@ -1,6 +1,6 @@
 let pkgs = builtins.fetchGit {
       url = https://github.com/samuelrivas/nixpkgs.git;
-      name = "nxipkgs-patched";
+      name = "nixpkgs-patched";
       ref = "fix-color-theme-solarized";
       rev = "fdb00fdc2c146171e424bbe9263780462bb7964c";
     };
--- nix/nixpkgs-upstream.nix
+++ nix/nixpkgs-upstream.nix
@@ -1,6 +1,6 @@
 let pkgs =  builtins.fetchGit {
       url = https://github.com/NixOS/nixpkgs.git;
-      name = "nxipkgs-upstream";
+      name = "nixpkgs-upstream";
       ref = "master";
       rev = "f5670ebd8e793aeb599c8f49794b3dafc31c27fd";
     };
