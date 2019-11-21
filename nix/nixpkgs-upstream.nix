1c55ba749f857db5793861f928b7277c2aa96a5c
Samuel Rivas <samuelrivas@gmail.com>
2019-11-21 20:16:10 +0100

    Advance upstream

--- nix/nixpkgs-upstream.nix
+++ nix/nixpkgs-upstream.nix
@@ -2,6 +2,6 @@ let pkgs =  builtins.fetchGit {
       url = https://github.com/NixOS/nixpkgs.git;
       name = "nixpkgs-upstream";
       ref = "master";
-      rev = "f5670ebd8e793aeb599c8f49794b3dafc31c27fd";
+      rev = "bb9da1323ebe39ff98a8412b963078a3a2bd2687";
     };
 in import pkgs
