ab236fdae297025ad5395dbac8836f854bf0a634
Samuel Rivas <samuelrivas@gmail.com>
2019-11-21 22:03:30 +0100

    Let's see if travis builds these days

--- travis-install.sh
+++ travis-install.sh
@@ -4,9 +4,6 @@ set -euo pipefail
 
 export NIX_CURL_FLAGS=-sS
 
-readonly PKGS_DIR="/tmp/upstream-pkgs"
-readonly PKGS_SHA="9553ca5bb6c01bb72421eb85932feb753c3ec3de"
-
 install_nix() {
     curl -sS https://nixos.org/nix/install > install-nix
     bash ./install-nix
@@ -18,14 +15,9 @@ create_config() {
     cat > "$config_dir/configuration.nix"  <<EOF
 {
   emacs-config = {
-    org-agenda-file = /dev/null;
-    org-template-dir = /dev/null;
-    org-meeting-file = /dev/null;
-    org-interview-file = /dev/null;
-    org-todo-file = /dev/null;
-  };
-  upstream-pkgs = {
-    dir = $PKGS_DIR/nixpkgs;
+    full-user-name = "Test user";
+    blacklisted-modes = [ ];
+    extra-config = "";
   };
   sams-pkgs = {
     dir = "$PWD/nix";
@@ -34,29 +26,8 @@ create_config() {
 EOF
 }
 
-install_upstream_nixpkgs() {
-    mkdir $PKGS_DIR
-    curl -sSL "https://github.com/NixOS/nixpkgs/archive/$PKGS_SHA.zip" \
-         > $PKGS_DIR/nixpkgs.zip
-    cd $PKGS_DIR
-    unzip nixpkgs.zip > /dev/null
-    mv "nixpkgs-$PKGS_SHA" nixpkgs
-}
-
-reset_channel() {
-    nix-channel --remove nixpkgs
-    nix-channel --add https://nixos.org/channels/nixos-17.03 nixpkgs
-    nix-channel --update
-}
-
 main() {
     install_nix
-
-    source "$HOME/.nix-profile/etc/profile.d/nix.sh"
-
-    reset_channel
-
-    install_upstream_nixpkgs
     create_config
 }
 
