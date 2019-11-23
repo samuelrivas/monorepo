c7593a747a0dbfe74097911cd8ac8642d707c5e7
Samuel Rivas <samuelrivas@gmail.com>
2019-11-23 08:13:32 +0100

    we don't need this any more

--- travis-test.sh
+++ travis-test.sh
@@ -4,19 +4,8 @@ set -euo pipefail
 
 export NIX_CURL_FLAGS=-sS
 
-source "$HOME/.nix-profile/etc/profile.d/nix.sh"
-
-info() {
-    echo "NIX_PATH=$NIX_PATH"
-}
-
-build_all() {
-    nix-build -A pkgs-sam nix
-}
-
 main() {
-    info
-    build_all
+    nix-build -A pkgs-sam nix
 }
 
 main
