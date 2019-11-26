5584e5871a906c8704768f4e7e1ef8f3f0946bb7
Samuel Rivas <samuelrivas@gmail.com>
2019-11-26 08:44:03 +0100

    update nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -2,6 +2,6 @@ let pkgs = builtins.fetchGit {
       url = https://github.com/NixOS/nixpkgs.git;
       name = "nixpkgs-stable";
       ref = "nixos-19.09";
-      rev = "c0c062da7b3e20ec3b6c9416bc03cb3fc61df4b8";
+      rev = "4ad6f1404a8cd69a11f16edba09cc569e5012e42";
     };
 in import pkgs
