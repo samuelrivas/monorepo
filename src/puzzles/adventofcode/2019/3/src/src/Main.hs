50a4040aaeb762108ef1e69a2faa16325766efb4
Samuel Rivas <samuelrivas@gmail.com>
2019-12-03 08:23:29 +0100

    Day 3 solution 1

---
+++ src/puzzles/adventofcode/2019/3/src/src/Main.hs
@@ -0,0 +1,99 @@
+-- {-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
+-- {-# OPTIONS_GHC -fno-warn-unused-imports #-}
+-- {-# OPTIONS_GHC -fno-warn-orphans #-}
+
+{-# LANGUAGE DerivingStrategies  #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedLabels    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import           Prelude       hiding (getLine)
+
+import           Control.Lens  (over, _1, _2)
+import           Data.Foldable (concatMap, foldl')
+import           Data.Map      (Map, empty, insert, keysSet)
+import           Data.Set      (Set, intersection)
+import qualified Data.Set      as Set
+import           Data.Text     (Text, splitOn, unpack)
+import qualified Data.Text     as Text
+import           Data.Text.IO  (getLine)
+
+{-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
+
+data Move = U | D | R | L
+  deriving stock Show
+
+type Grid = Map (Int, Int) Bool
+type Pos = (Int, Int)
+
+example_easy :: (Text, Text)
+example_easy = ("R8,U5,L5,D3",
+                "U7,R6,D4,L4")
+
+example_1 :: (Text, Text)
+example_1 = ("R75,D30,R83,U83,L12,D49,R71,U7,L72",
+             "U62,R66,U55,R34,D71,R55,D58,R83")
+
+example_2 :: (Text, Text)
+example_2 = ("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+             "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+
+parse :: Text -> Maybe [(Move, Int)]
+parse input =
+  let codes = (,) <$> Text.head <*> Text.tail <$> splitOn "," input
+  in sequence $ parse_code <$> codes
+
+expand :: [(Move, Int)] -> [Move]
+expand = concatMap (\(m,d) -> replicate d m)
+
+parse_code :: (Char, Text) -> Maybe (Move, Int)
+parse_code (m, u) = do
+    move <- case m of
+              'R' -> Just R
+              'L' -> Just L
+              'D' -> Just D
+              'U' -> Just U
+              _   -> Nothing
+    pure (move, read . unpack $ u)
+
+get_new_pos :: (Move, Int) -> Pos -> Pos
+get_new_pos (move, distance) =
+  case move of
+    U -> over _2 (+ distance)
+    D -> over _2 (+ (-distance))
+    R -> over _1 (+ distance)
+    L -> over _1 (+ (-distance))
+
+lay_cable :: [Move] -> Grid
+lay_cable =
+  let
+    step (pos, grid) move =
+      let newpos = get_new_pos (move, 1) pos
+      in (newpos, insert newpos True grid)
+  in
+    snd . foldl' step ((0, 0), empty)
+
+get_crosses :: Grid -> Grid -> Set Pos
+get_crosses x y = intersection (keysSet x) (keysSet y)
+
+manhattan :: Pos -> Pos -> Int
+manhattan (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+
+cross_cables :: [(Move, Int)] -> [(Move, Int)] -> Set Pos
+cross_cables x y =
+  let
+    cable_x = lay_cable . expand $ x
+    cable_y = lay_cable . expand $ y
+  in
+    get_crosses cable_x cable_y
+
+find_solution :: [(Move, Int)] -> [(Move, Int)] -> Int
+find_solution x y =
+  Set.findMin . Set.map (manhattan (0, 0)) $ cross_cables x y
+
+main :: IO ()
+main = do
+  Just x <- parse <$> getLine
+  Just y <- parse <$> getLine
+  putStrLn $ "Solution 1: " <> show (find_solution x y)
