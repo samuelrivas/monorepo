d34793210d6dab3c99c0d99d52fcf018b2ec412d
Samuel Rivas <samuelrivas@gmail.com>
2019-12-04 08:48:40 +0100

    solution 2

--- src/puzzles/adventofcode/2019/4/src/src/Main.hs
+++ src/puzzles/adventofcode/2019/4/src/src/Main.hs
@@ -22,6 +22,12 @@ has_double l = any (uncurry  (==)) $ zip l (tail l)
 is_ascending :: Ord a => [a] -> Bool
 is_ascending l = all (uncurry  (<=)) $ zip l (tail l)
 
+has_single_double :: Eq a => [a] -> Bool
+has_single_double (h : l) =
+  ((length . takeWhile (== h) $ l) == 1)
+  || has_single_double (dropWhile (== h) l)
+has_single_double _ = False
+
 valid :: Int -> Bool
 valid x =
   let
@@ -29,6 +35,14 @@ valid x =
   in
     has_double digits && is_ascending digits
 
+valid_2 :: Int -> Bool
+valid_2 x =
+  let
+    digits = factor x
+  in
+    has_single_double digits && is_ascending digits
+
 main :: IO ()
-main =
+main = do
   putStrLn $ "Solution 1: " <> show (length $ filter valid [256310..732736])
+  putStrLn $ "Solution 1: " <> show (length $ filter valid_2 [256310..732736])
