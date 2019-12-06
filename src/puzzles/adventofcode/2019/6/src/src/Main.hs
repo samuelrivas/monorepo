cc325163489ba2bee4dac8ec9773e8c77475b9ea
Samuel Rivas <samuelrivas@gmail.com>
2019-12-06 21:22:29 +0100

    final cleanup

--- src/puzzles/adventofcode/2019/6/src/src/Main.hs
+++ src/puzzles/adventofcode/2019/6/src/src/Main.hs
@@ -1,44 +1,32 @@
-{-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
-{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+-- {-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
+-- {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 -- {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-{-# LANGUAGE DerivingStrategies         #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase                 #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE OverloadedLabels           #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedLabels      #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
 
-import           Prelude               hiding (getLine, lines, putStrLn,
-                                        unlines, readFile)
+import           Prelude              hiding (getLine, lines, putStrLn,
+                                       readFile, unlines)
 
-import           Control.Lens          (assign, ix, modifying, preview, use,
-                                        uses, toListOf, both, over)
-import           Control.Monad         (when, join)
-import           Control.Monad.Loops   (whileM_)
-import           Control.Monad.Reader  (MonadReader)
-import           Control.Monad.RWS.CPS (RWST, evalRWST, execRWST, get, put,
-                                        runRWST, tell)
-import           Control.Monad.State   (MonadState)
-import           Control.Monad.Writer  (MonadWriter)
-import           Data.Generics.Labels  ()
-import           Data.List             (uncons)
-import           Data.Maybe            (fromMaybe)
-import           Data.Text             (Text, lines, pack, splitOn, unlines,
-                                        unpack)
-import           Data.Text.IO          (getLine, putStrLn, readFile)
-import Data.Graph
-import Data.Map.Strict ((!), Map, findMin, findMax, elems)
-import Data.Set (Set, notMember)
-import qualified Data.Set as Set
-import qualified Data.Map.Strict as Map
-import Data.Tuple (swap)
-import Data.Foldable (find)
-import qualified Data.Array as Array
-import Data.Maybe (isJust, fromJust)
+import           Control.Lens         (both, over, toListOf)
+import           Control.Monad        (join)
+import qualified Data.Array           as Array
+import           Data.Foldable        (find)
+import           Data.Generics.Labels ()
+import           Data.Graph           (Graph, buildG, reachable)
+import           Data.Map.Strict      (Map, elems, findMax, findMin, (!))
+import qualified Data.Map.Strict      as Map
+import           Data.Maybe           (fromJust, isJust)
+import           Data.Set             (Set, notMember)
+import qualified Data.Set             as Set
+import           Data.Text            (Text, lines, pack, splitOn, unlines)
+import           Data.Text.IO         (putStrLn, readFile)
+import           Data.Tuple           (swap)
 
 {-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
 
@@ -48,13 +36,14 @@ type Index = (Map Int Text, Map Text Int)
 test_input :: Text
 test_input =
   unlines
-  ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"]
+  ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G",
+   "G)H", "D)I", "E)J", "J)K", "K)L"]
 
 test_input_2 :: Text
 test_input_2 =
   unlines
-  ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L",
-   "K)YOU", "I)SAN"]
+  ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G",
+   "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN"]
 
 parse_orbit :: Text -> Maybe (Text, Text)
 parse_orbit input = do
@@ -97,9 +86,10 @@ get_undirected_graph orbits idx@(vertex_to_object, _) =
 count_orbits :: Index -> Graph -> Int
 count_orbits idx g =
   let
-    vertices' = elems $ snd idx
-    reachables = reachable g <$> vertices'
+    vertices = elems $ snd idx
+    reachables = reachable g <$> vertices
   in
+    -- Subtract one since an object counts as reachable from itself
     sum $ (+ (-1)) . length <$> reachables
 
 solution_1 :: [(Text, Text)] -> Int
@@ -110,16 +100,21 @@ solution_1 orbits =
   in
     count_orbits idx g
 
--- DFS until we reach destination
-path' :: Graph -> Int -> Int -> Maybe [Int]
-path' g orig dest = aux_path g orig dest [] Set.empty
+-- Find the shortest path from orig to dest, both included int he result
+path :: Graph -> Int -> Int -> Maybe [Int]
+path dest orig = aux_path [] Set.empty dest orig
 
-aux_path :: Graph -> Int -> Int -> [Int] -> Set Int -> Maybe [Int]
-aux_path g v dest acc visited
-  | v == dest = Just (reverse $ dest:acc)
+-- dfs algorithm
+aux_path :: [Int] -> Set Int -> Graph -> Int -> Int -> Maybe [Int]
+aux_path acc visited g dest orig
+  | orig == dest = Just (reverse $ dest:acc)
   | otherwise =
-    join $ find isJust $ fmap (\v' -> aux_path g v' dest (v:acc) (Set.insert v visited))
-     $ filter (`notMember` visited) (g Array.! v)
+    let
+      hops = filter (`notMember` visited) (g Array.! orig)
+      sub_path = aux_path (orig:acc) (Set.insert orig visited) g dest
+      paths = sub_path <$> hops
+    in
+      join $ find isJust paths
 
 solution_2 :: [(Text, Text)] -> Maybe Int
 solution_2 orbits =
@@ -128,13 +123,13 @@ solution_2 orbits =
     g = get_undirected_graph orbits idx
     orig = to_vertex idx "YOU"
     dest = to_vertex idx "SAN"
-
   in
-    (+ (-3)) . length  <$> path' g orig dest
+    -- Subtract 3 as we are including but us and santa, plus there is one less
+    -- transfer than elements in the rest of the path
+    (+ (-3)) . length  <$> path g dest orig
 
 main :: IO ()
 main = do
   Just orbits <- parse_input <$> readFile "input.txt"
   putStrLn $ "Solution 1: " <> (pack . show . solution_1 $ orbits)
   putStrLn $ "Solution 2: " <> (pack . show . fromJust . solution_2 $ orbits)
-  
