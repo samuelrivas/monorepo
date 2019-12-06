4f212cb3c7a1caa673184c12931948bf16dc4119
Samuel Rivas <samuelrivas@gmail.com>
2019-12-06 21:00:52 +0100

    and this solves problem 2

--- src/puzzles/adventofcode/2019/6/src/src/Main.hs
+++ src/puzzles/adventofcode/2019/6/src/src/Main.hs
@@ -17,7 +17,7 @@ import           Prelude               hiding (getLine, lines, putStrLn,
 
 import           Control.Lens          (assign, ix, modifying, preview, use,
                                         uses, toListOf, both, over)
-import           Control.Monad         (when)
+import           Control.Monad         (when, join)
 import           Control.Monad.Loops   (whileM_)
 import           Control.Monad.Reader  (MonadReader)
 import           Control.Monad.RWS.CPS (RWST, evalRWST, execRWST, get, put,
@@ -32,10 +32,13 @@ import           Data.Text             (Text, lines, pack, splitOn, unlines,
 import           Data.Text.IO          (getLine, putStrLn, readFile)
 import Data.Graph
 import Data.Map.Strict ((!), Map, findMin, findMax, elems)
-import Data.Set (Set)
+import Data.Set (Set, notMember)
 import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
 import Data.Tuple (swap)
+import Data.Foldable (find)
+import qualified Data.Array as Array
+import Data.Maybe (isJust, fromJust)
 
 {-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
 
@@ -46,6 +49,12 @@ test_input :: Text
 test_input =
   unlines
   ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"]
+
+test_input_2 :: Text
+test_input_2 =
+  unlines
+  ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L",
+   "K)YOU", "I)SAN"]
 
 parse_orbit :: Text -> Maybe (Text, Text)
 parse_orbit input = do
@@ -77,6 +86,14 @@ get_graph orbits idx@(vertex_to_object, _) =
   in
     buildG bounds edges'
 
+get_undirected_graph :: [(Text, Text)] -> Index -> Graph
+get_undirected_graph orbits idx@(vertex_to_object, _) =
+  let
+    bounds = (fst $ findMin vertex_to_object, fst $ findMax vertex_to_object)
+    edges' = over (traverse . both) (to_vertex idx) orbits
+  in
+    buildG bounds (edges' ++ (swap <$> edges'))
+
 count_orbits :: Index -> Graph -> Int
 count_orbits idx g =
   let
@@ -93,7 +110,31 @@ solution_1 orbits =
   in
     count_orbits idx g
 
+-- DFS until we reach destination
+path' :: Graph -> Int -> Int -> Maybe [Int]
+path' g orig dest = aux_path g orig dest [] Set.empty
+
+aux_path :: Graph -> Int -> Int -> [Int] -> Set Int -> Maybe [Int]
+aux_path g v dest acc visited
+  | v == dest = Just (reverse $ dest:acc)
+  | otherwise =
+    join $ find isJust $ fmap (\v' -> aux_path g v' dest (v:acc) (Set.insert v visited))
+     $ filter (`notMember` visited) (g Array.! v)
+
+solution_2 :: [(Text, Text)] -> Maybe Int
+solution_2 orbits =
+  let
+    idx = get_index orbits
+    g = get_undirected_graph orbits idx
+    orig = to_vertex idx "YOU"
+    dest = to_vertex idx "SAN"
+
+  in
+    (+ (-3)) . length  <$> path' g orig dest
+
 main :: IO ()
 main = do
   Just orbits <- parse_input <$> readFile "input.txt"
-  putStrLn $ "Solution 1: " <> (pack . show $ solution_1 orbits)
+  putStrLn $ "Solution 1: " <> (pack . show . solution_1 $ orbits)
+  putStrLn $ "Solution 2: " <> (pack . show . fromJust . solution_2 $ orbits)
+  
