812adefe3027cb3469fd4c6821307c0332690957
Samuel Rivas <samuelrivas@gmail.com>
2019-12-12 15:45:31 +0100

    12 Part 1

--- src/puzzles/adventofcode/2019/12/src/src/Main.hs
+++ src/puzzles/adventofcode/2019/12/src/src/Main.hs
@@ -2,42 +2,37 @@
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-{-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DerivingStrategies    #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedLabels      #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TupleSections         #-}
 
-import           Prelude               hiding (Left, Right, concat, getLine,
-                                        putStrLn, readFile, show)
-
-import           Control.Lens          (assign, at, modifying, non, over, set,
-                                        toListOf, traverse, use, view, _1, _2,
-                                        _3)
+import           Control.Lens          (assign, at, each, modifying, non, over,
+                                        set, sumOf, to, toListOf, traverse, use,
+                                        view, _1, _2, _3)
+import           Control.Monad         (replicateM_)
 import           Control.Monad.Loops   (whileM_)
-import           Control.Monad.State   (StateT, lift, runStateT)
-import           Data.Foldable         (maximum, minimum)
+import           Control.Monad.State   (State, execState, get, modify, put)
+import           Data.Foldable         (foldl')
 import           Data.Functor.Identity (runIdentity)
 import           Data.Generics.Labels  ()
 import           Data.Map.Strict       (Map, empty, keys, size)
 import           Data.Text             (Text, concat, intercalate, splitOn,
                                         unpack)
-import           Data.Text.IO          (putStrLn, readFile)
 import           GHC.Generics          (Generic)
-import Data.Foldable (foldl')
-import Control.Monad.State (State, get, modify, put)
 
-import Internal
+import           Internal
 
 {-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
 
 example_1 :: [Moon]
 example_1 = mk_moon <$> [(-1, 0, 2), (2, -10, -7), (4, -8, 8), (3, 5, -1)]
+
+example_2 :: [Moon]
+example_2 = mk_moon <$> [(-8, -10, 0), (5, 5, 10), (2, -7, 3), (9, -8, -3)]
 
 sum_coord :: Coord -> Coord -> Coord
 sum_coord (x1, y1, z1) (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2)
@@ -63,8 +58,10 @@ velocity_delta moons moon =
 
 update_moon :: Coord -> Moon -> Moon
 update_moon v moon =
-  over #pos (`sum_coord` v)
-  $ over #velocity (`sum_coord` v) moon
+  let new_v = sum_coord (view #velocity moon) v
+  in
+    set #velocity new_v
+    $ over #pos (sum_coord new_v) moon
 
 type ProblemMonad = State [Moon]
 
@@ -74,6 +71,24 @@ step = do
   let delta_vs = velocity_delta moons <$> moons
   modify $ zipWith update_moon delta_vs
 
-main :: IO ()
-main = undefined
+run_steps :: Int -> [Moon] -> [Moon]
+run_steps n  = execState (replicateM_ n step)
 
+energy :: Moon -> Integer
+energy m = sumOf (#pos . each . to abs) m
+           * sumOf (#velocity . each . to abs) m
+
+input :: [Moon]
+input = mk_moon <$> [(-10, -10, -13),
+                     (5, 5, -9),
+                     (3, 8, -16),
+                     (1, 3, -3)]
+
+solution_1 :: Integer
+solution_1 =
+  let moons = run_steps 1000 input
+  in sum $ energy <$> moons
+
+main :: IO ()
+main =
+  putStrLn $ "Solution 1: " <> show solution_1
