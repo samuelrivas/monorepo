a285ba2887537331c6427ea475e602b2df0bc32d
Samuel Rivas <samuelrivas@gmail.com>
2019-12-24 17:07:20 +0100

    seems the hyperbug is approaching

--- src/puzzles/adventofcode/2019/24/src/src/Main.hs
+++ src/puzzles/adventofcode/2019/24/src/src/Main.hs
@@ -29,6 +29,9 @@ import           Data.Generics.Labels   ()
 import  Data.HashSet          (HashSet)
 import qualified Data.HashSet           as HashSet
 import qualified Data.Map.Strict        as Map
+import qualified Data.HashMap.Strict        as HashMap
+import  Data.HashMap.Strict        ( HashMap)
+import  Data.Map.Strict        ( Map)
 import qualified Data.Text as Text
 import           Data.Maybe             (catMaybes, fromJust)
 import           Data.Text              (Text, pack)
@@ -63,7 +66,7 @@ parseInput = Map.map (== '#') . fromText
 showCell :: Maybe Bool -> Text
 showCell = maybe "?" (bool "." "#")
 
-readCell :: Bidim Bool -> Coord -> Bool
+readCell :: Ord a => Map a Bool -> a -> Bool
 readCell eris p = Map.findWithDefault False p eris
 
 updateCell :: Bidim Bool -> Coord -> Bool -> Bool
@@ -77,10 +80,105 @@ updateCell eris pos isBug =
       (n, False) | 1 <= n && n <= 2 -> True
       (_, _) -> isBug
 
+updateHyperCell :: HyperEris -> HyperCoord -> Bool -> Bool
+updateHyperCell _ ((2, 2), _) _ = False
+updateHyperCell eris pos isBug =
+  let
+    neighbors = readCell eris <$> hyperCross pos
+    numBugs = sum $ fromEnum <$> neighbors
+  in
+    case (numBugs, isBug) of
+      (n, True) | n /= 1 -> False
+      (n, False) | 1 <= n && n <= 2 -> True
+      (_, _) -> isBug
+
 minute :: Bidim Bool -> Bidim Bool
 minute eris = Map.mapWithKey (updateCell eris) eris
 
+hyperMinute :: HyperEris -> HyperEris
+hyperMinute eris = Map.mapWithKey (updateHyperCell eris) eris
+
+project :: Integer -> HyperEris -> Bidim Bool
+project level hyperEris =
+  let onlyLevel hyperCoord _ = snd hyperCoord == level
+  in Map.mapKeys fst $ Map.filterWithKey onlyLevel hyperEris
+
+inject :: Integer -> Bidim Bool -> HyperEris
+inject level = Map.mapKeys (, level)
+
 type ErisT = StateT (HashSet Text)
+
+-- Coord and level
+type HyperCoord = (Coord, Integer)
+
+type HyperEris = Map HyperCoord Bool
+
+row :: Integer -> Integer -> [HyperCoord]
+row = undefined
+
+column :: Integer -> Integer -> [HyperCoord]
+column = undefined
+
+inBounds :: Coord -> Bool
+inBounds coord@(x, y) =
+  let
+    valid a = (a >= 0) && (4 >= a)
+    center = (2, 2)
+  in
+    valid x && valid y && coord /= center
+
+toHyperCoord :: Integer -> Coord -> HyperCoord
+toHyperCoord level coord = (coord, level)
+
+hyperCross :: HyperCoord -> [HyperCoord]
+hyperCross hyperCoord@(coord, level) =
+  let
+    fromLevel  = toHyperCoord level <$> filter inBounds (cross coord)
+  in
+    HashSet.toList . HashSet.fromList .
+    concat $ [
+      fromLevel,
+      innerTop hyperCoord,
+      innerLeft hyperCoord,
+      innerBottom hyperCoord,
+      innerRight hyperCoord,
+      outerTop hyperCoord,
+      outerLeft hyperCoord,
+      outerBottom hyperCoord,
+      outerRight hyperCoord
+      ]
+
+innerTop :: HyperCoord -> [HyperCoord]
+innerTop ((2, 1), level) = (, level - 1) . (, 0) <$> [0..4]
+innerTop _ = []
+
+innerBottom :: HyperCoord -> [HyperCoord]
+innerBottom ((2, 3), level) = (, level - 1) . (, 4) <$> [0..4]
+innerBottom _ = []
+
+innerLeft :: HyperCoord -> [HyperCoord]
+innerLeft ((1, 2), level) = (, level - 1) . (0, ) <$> [0..4]
+innerLeft _ = []
+
+innerRight :: HyperCoord -> [HyperCoord]
+innerRight ((3, 2), level) = (, level - 1) . (4, ) <$> [0..4]
+innerRight _ = []
+
+outerTop :: HyperCoord -> [HyperCoord]
+outerTop ((_, 4), level) = [((2, 0), level + 1)]
+outerTop _ = []
+
+outerBottom :: HyperCoord -> [HyperCoord]
+outerBottom ((_, 0), level) = [((2, 4), level + 1)]
+outerBottom _ = []
+
+outerLeft :: HyperCoord -> [HyperCoord]
+outerLeft ((4, _), level) = [((0, 2), level + 1)]
+outerLeft _ = []
+
+outerRight :: HyperCoord -> [HyperCoord]
+outerRight ((0, _), level) = [((4, 2), level + 1)]
+outerRight _ = []
 
 findDup :: Monad m => Bidim Bool -> ErisT m (Bidim Bool)
 findDup eris =
