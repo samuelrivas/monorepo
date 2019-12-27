d042fd44f51f1ee3f678f3a4db8640db6c35e36b
Samuel Rivas <samuelrivas@gmail.com>
2019-12-27 07:46:12 +0100

    23 part 2, should work, but doesn't

--- src/puzzles/adventofcode/2019/23/src/src/Internal.hs
+++ src/puzzles/adventofcode/2019/23/src/src/Internal.hs
@@ -17,20 +17,22 @@ module Internal (
   NodeState (NodeState)
   ) where
 
-import           Control.Lens  (ix, preview)
-import           Data.Hashable (Hashable)
-import           Data.HashMap.Strict  (HashMap)
-import           Data.Maybe    (fromJust)
-import           GHC.Generics  (Generic)
+import           Data.HashMap.Strict (HashMap)
+import           Data.Monoid         (Sum)
+import           GHC.Generics        (Generic)
 
-import Intcode
+import           Intcode
 
 data NodeState = NodeState {
   computerState :: IntcodeState,
-  input :: Maybe [Integer]
+  input         :: Maybe [Integer]
   } deriving stock (Show, Generic)
 
 data NetworkState = NetworkState {
-  nodes :: HashMap Integer NodeState,
-  nat :: Maybe (Integer, Integer)
+  nodes     :: HashMap Integer NodeState,
+  nat       :: Maybe (Integer, Integer),
+  lastNat   :: Maybe (Integer, Integer),
+  idle      :: Bool,
+  dupNats   :: [Integer],
+  idleCount :: Sum Integer
   } deriving stock (Show, Generic)
--- src/puzzles/adventofcode/2019/23/src/src/Main.hs
+++ src/puzzles/adventofcode/2019/23/src/src/Main.hs
@@ -11,37 +11,39 @@
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 
-import           Prelude                 hiding (Left, Right, concat, getLine,
-                                          putStr, putStrLn, readFile, show)
+import           Prelude                   hiding (Left, Right, concat, getLine,
+                                            putStr, putStrLn, readFile, show)
 import qualified Prelude
 
-import           Control.Applicative     ((<|>))
-import           Control.Lens            (uses, assign, at, both, ix, modifying,
-                                          productOf, set, use, view, views, _1,
-                                          _2, _Just)
-import Data.List (unfoldr)
-import           Control.Monad           (when)
-import           Control.Monad.IO.Class  (liftIO)
-import           Control.Monad.Loops     (untilJust)
-import           Control.Monad.RWS.CPS   (RWST, evalRWST, execRWST, lift, tell)
-import Control.Monad.State -- close this
-import           Data.Foldable           (fold, foldl')
-import           Data.Functor.Identity   (runIdentity)
-import           Data.Generics.Labels    ()
-import           Data.List               (find, maximumBy, sort, tails)
-import           Data.Map.Strict         (Map, empty, insert, keys, toList)
-import           Data.Maybe              (fromJust, fromMaybe)
-import           Data.Sequence           (Seq ((:<|)), fromList, (><), (|>))
-import qualified Data.Sequence           as Seq
-import           Data.Text               (Text, pack, splitOn, unpack)
-import           Data.Text.IO            (putStr, putStrLn, readFile)
-import           System.Console.Readline (readline)
-import Control.Monad.Trans.Maybe (runMaybeT, MaybeT)
-import Data.HashMap.Strict (HashMap, (!))
-import qualified Data.HashMap.Strict as HashMap
+import           Control.Applicative       ((<|>))
+import           Control.Lens              (assign, at, both, ix, modifying,
+                                            productOf, set, use, uses, view,
+                                            views, _1, _2, _Just)
+import           Control.Monad             (when)
+import           Control.Monad.IO.Class    (liftIO)
+import           Control.Monad.Loops       (untilJust, whileM)
+import           Control.Monad.RWS.CPS     (RWST, evalRWST, execRWST, lift,
+                                            tell)
+import           Control.Monad.State
+import           Control.Monad.Trans.Maybe (MaybeT, runMaybeT)
+import           Data.Foldable             (fold, foldl')
+import           Data.Functor.Identity     (runIdentity)
+import           Data.Generics.Labels      ()
+import           Data.HashMap.Strict       (HashMap, (!))
+import qualified Data.HashMap.Strict       as HashMap
+import           Data.List                 (unfoldr)
+import           Data.List                 (find, maximumBy, sort, tails)
+import           Data.Map.Strict           (Map, empty, insert, keys, toList)
+import           Data.Maybe                (fromJust, fromMaybe)
+import           Data.Monoid               (Sum (..))
+import           Data.Sequence             (Seq ((:<|)), fromList, (><), (|>))
+import qualified Data.Sequence             as Seq
+import           Data.Text                 (Text, pack, splitOn, unpack)
+import           Data.Text.IO              (putStr, putStrLn, readFile)
+import           System.Console.Readline   (readline)
 
 import           Intcode
-import Internal
+import           Internal
 
 type NetworkT = StateT NetworkState
 
@@ -75,10 +77,10 @@ initialNetworkState intcode =
     intcodeState addr = view _2 . runIdentity $ run (initComputer addr) intcode
     nodes = (\addr -> (addr, mkNodeState . intcodeState $ addr)) <$> allNodes
   in
-    NetworkState (HashMap.fromList nodes) Nothing
+    NetworkState (HashMap.fromList nodes) Nothing Nothing True [] mempty
 
 computerStep ::
-  Monad m => Maybe [Integer] -> IntcodeT m ([Integer], Status)
+  Monad m => Maybe [Integer] -> IntcodeT m ([Integer], Status, Bool)
 computerStep inputM = do
   runProgram
   output <- getOutput
@@ -89,25 +91,34 @@ computerStep inputM = do
   when (status == Interrupted) $
     pushInput (fromMaybe [-1] inputM)
 
-  pure (output, status)
+  input <- use #input -- We are breaking encapsulation here, but whatever..
+
+  pure (output, status, input == [-1])
 
 -- Returns packages sent
-runNode :: Monad m => Integer -> NetworkT m [Integer]
+runNode :: Monad m => Integer -> NetworkT m ()
 runNode addr = do
   nodeState <- uses #nodes (! addr)
   let computerState = view #computerState nodeState
       input = view #input nodeState
 
-  ((output, _status), newState, _w) <-
+  ((output, _status, noInput), newState, _w) <-
     runEmpty (reset computerState >> computerStep input)
+
+  when (_status /= Interrupted) $ do
+    error "unexpected status"
+    pure ()
 
   let newNode = set #computerState newState $
                 set #input Nothing nodeState
 
   assign (#nodes . at addr) $ Just newNode
 
+  unless (null output && noInput) $ do
+    assign #idleCount (Sum 0)
+    assign #idle False
+
   dispatchOutput output
-  pure output
 
 dispatchOutput :: Monad m =>  [Integer] -> NetworkT m ()
 dispatchOutput [] = pure ()
@@ -122,8 +133,42 @@ dispatchOutput (addr : x : y : t) = do
   dispatchOutput t
 dispatchOutput other = error $ "weird output: " ++ Prelude.show other
 
-networkStep :: Monad m => NetworkT m [[Integer]]
-networkStep = sequence $ runNode <$> [0..49]
+networkStep :: Monad m => NetworkT m ()
+networkStep = do
+  assign #idle True
+  sequence_ $ runNode <$> [0..49]
+  natCheck
+
+natCheck :: Monad m => NetworkT m ()
+natCheck = do
+  isIdle <- use #idle
+  when isIdle $ modifying #idleCount (Sum 1 <>)
+
+  count <- use #idleCount
+  when (getSum count > 3) $
+    use #nat >>= \case
+      Just (x, y) -> do
+        assign #lastNat (Just (x, y))
+        assign #idleCount mempty
+        dispatchOutput [0, x, y]
+        sniffNat y
+      Nothing -> error "nonat"
+
+sniffNat :: Monad m => Integer -> NetworkT m ()
+sniffNat y = do
+  dups <- use #dupNats
+  use #dupNats >>= \case
+    [] -> assign #dupNats [y]
+    [x] | x == y -> assign #dupNats [y, y]
+        | otherwise -> assign #dupNats [y]
+    _ -> error $ "sniffed too much: " <> Prelude.show dups
+  -- modifying #dupNats (y :)
+
+monitorNat :: Monad m => NetworkT m (Maybe (Integer, Integer))
+monitorNat = do
+  lastNat <- use #lastNat
+  assign #lastNat Nothing
+  pure lastNat
 
 initComputer :: Monad m => Integer -> IntcodeT m ()
 initComputer = pushInput . pure
@@ -142,13 +187,13 @@ main = do
 
 -- Testing
 initialisedIntcode :: Integer -> IO IntcodeState
-initialisedIntcode id = view _2 <$> (getInput >>= run (initComputer id))
+initialisedIntcode addr = view _2 <$> (getInput >>= run (initComputer addr))
 
 debugNetworkState :: IO NetworkState
 debugNetworkState = do
   intcodeState <- initialisedIntcode 1
   let nodeState = mkNodeState intcodeState
-  pure $ NetworkState (HashMap.singleton 1 nodeState) Nothing
+  pure $ NetworkState (HashMap.singleton 1 nodeState) Nothing Nothing True [] mempty
 
 mkNodeState :: IntcodeState -> NodeState
 mkNodeState intcodeState = NodeState intcodeState Nothing
