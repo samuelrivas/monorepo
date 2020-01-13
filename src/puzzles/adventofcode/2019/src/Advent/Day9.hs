4450f3e4e70c7318c4b4298f5566931339eacf19
Samuel Rivas <samuelrivas@gmail.com>
2020-01-13 10:56:22 +0100

    one less Intcode to go

--- src/puzzles/adventofcode/2019/src/Advent/Day9.hs
+++ src/puzzles/adventofcode/2019/src/Advent/Day9.hs
@@ -6,40 +6,42 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
 {-# LANGUAGE OverloadedLabels      #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
+
 module Advent.Day9 where
 
-import           Prelude               hiding (getLine, putStrLn, readFile)
+import           Perlude
 
-import           Control.Lens          (view, _3)
+import           Control.Lens          (view, _1)
 import           Data.Functor.Identity (runIdentity)
-import           Data.Generics.Labels  ()
-import           Data.Text             (Text, splitOn, unpack)
-import           Data.Text.IO          (putStrLn)
 
-import           Advent.Day9.Intcode
-import           System.IO.Advent      (getInput)
+import           Control.Monad.Intcode
 
-{-# ANN module ("HLint: ignore Use camelCase" :: String) #-}
-example_1 :: [Integer]
-example_1 = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+example1 :: [Integer]
+example1 = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
 
-example_2 :: [Integer]
-example_2 = [1102,34915192,34915192,7,4,7,99,0]
+example2 :: [Integer]
+example2 = [1102,34915192,34915192,7,4,7,99,0]
 
-example_3 :: [Integer]
-example_3 = [104,1125899906842624,99]
+example3 :: [Integer]
+example3 = [104,1125899906842624,99]
 
-solution_1 :: [Integer] -> Text
-solution_1 = view _3 . runIdentity . launch (push_input [1] >> run_program)
+solution :: Integer -> [Integer] -> Integer
+solution input =
+  head . view _1 . runIdentity .
+  eval (pushInput [input] >> runProgram >> getOutput)
 
-solution_2 :: [Integer] -> Text
-solution_2 = view _3 . runIdentity . launch (push_input [2] >> run_program)
+solution1 :: [Integer] -> Integer
+solution1 = solution 1
+
+solution2 :: [Integer] -> Integer
+solution2 = solution 2
 
 main :: IO ()
 main = do
-  code :: [Integer] <- fmap (read . unpack) . splitOn "," <$> getInput "9"
-  putStrLn $ "Solution 1: " <> solution_1 code
-  putStrLn $ "Solution 2: " <> solution_2 code
+  code :: [Integer] <- codeForDay "9"
+  putStrLn $ "Solution 1: " <> show (solution1 code)
+  putStrLn $ "Solution 2: " <> show (solution2 code)
