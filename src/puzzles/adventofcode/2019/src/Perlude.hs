5538194899cdf4cb79641bd0ca40da28e07f7892
Samuel Rivas <samuelrivas@gmail.com>
2020-01-13 11:29:11 +0100

    adapted day 7 to the common Intcode machine

--- src/puzzles/adventofcode/2019/src/Perlude.hs
+++ src/puzzles/adventofcode/2019/src/Perlude.hs
@@ -1,14 +1,26 @@
+-- A slightly adapted Prelude that you can import instead
+--
+-- The main purpose is to change common function signatures from 'String' to
+-- 'Text', but there are a few more modifictions:
+--
+-- Export 'Generic' and the instances for 'IsLabel' for all instances of Generic
+-- so that we can use lenses with @OverloadedLabels@ without adding noise to the
+-- import list
+--
+-- Export 'pack' and 'unpack' from Text to relieve a bit the pain of dealing
+-- with interfaces using Strings
 module Perlude (
   show,
   showStr,
+  error,
   module X,
   ) where
 
 import           Data.Generics.Labels as X ()
-import           Data.Text            as X (Text, pack)
+import           Data.Text            as X (Text, pack, unpack)
 import           Data.Text.IO         as X (putStrLn)
 import           GHC.Generics         as X (Generic)
-import           Prelude              as X hiding (putStrLn, show)
+import           Prelude              as X hiding (error, putStrLn, show)
 import qualified Prelude
 
 show :: Show a => a -> Text
@@ -16,3 +28,6 @@ show = pack . Prelude.show
 
 showStr :: Show a => a -> String
 showStr = Prelude.show
+
+error :: Text -> a
+error = Prelude.error . unpack
