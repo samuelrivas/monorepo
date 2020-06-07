d766be5ff1a23f3837455535222d3bdbd7be38e8
Samuel Rivas <samuelrivas@gmail.com>
2020-06-07 10:50:13 +0200

    update to the latest release

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -2,6 +2,6 @@ let pkgs = builtins.fetchGit {
       url = https://github.com/NixOS/nixpkgs.git;
       name = "nixpkgs-stable";
       ref = "nixos-20.03";
-      rev = "a53ed231d21803b61ca3cb10cedef9b7c09a2503";
+      rev = "1aa5271117107032e13f07bf025e3c4d26db8915";
     };
 in import pkgs
