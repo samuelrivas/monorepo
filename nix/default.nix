70e9d0ea10740bc7ecbcc2d91697bc3724be8dcf
Samuel Rivas <samuelrivas@gmail.com>
2020-08-15 09:15:06 +0200

    move haskell stuff to haskell.nix

--- nix/default.nix
+++ nix/default.nix
@@ -84,15 +84,13 @@ let
         emacs-config-options = pkgs-sam.local-config.emacs-config;
       });
 
-    # A utility to instantiate a capable emacs in a haskell sandbox
-    emacs-for-haskell = haskell-env: pkgs-sam.emacs.override { ghc = haskell-env; };
-
     # aspell needs to be configured to find the dictionaries
     aspell-wrapped = callPackage ./pkgs/development/libraries/aspell-wrapped { };
 
     # Haskell stuff
     # =============
     haskell-mk = callPackage ./../src/haskell/haskell-mk/nix {  };
+    haskell-lib = import ./lib/haskell.nix { inherit pkgs pkgs-sam; };
 
     profiledHaskellPackages = pkgs.haskellPackages.override {
       overrides = pkgs-sam: super: {
@@ -101,45 +99,7 @@ let
         });
       };
     };
-
-    haskell-pkg =
-      { haskellPackages ? pkgs.haskellPackages,
-        name,
-        src,
-        wanted-packages,
-        extra-build-inputs ? [],
-      }:
-      let
-        haskell-packages-selector = _: wanted-packages;
-        ghc = haskellPackages.ghcWithPackages haskell-packages-selector;
-      in
-        pkgs.stdenv.mkDerivation rec {
-
-          inherit name src;
-
-          buildInputs = [
-            ghc
-            pkgs-sam.haskell-mk
-          ] ++ extra-build-inputs;
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp ../build/bin/* $out/bin
-          '';
-
-          meta = {
-            inherit haskellPackages ghc;
-          };
-        };
-
-    haskell-shell = haskell-drv:
-      haskell-drv.overrideAttrs (attrs:
-
-        { buildInputs = attrs.buildInputs ++ [
-            (pkgs-sam.emacs-for-haskell haskell-drv.meta.ghc)
-            haskell-drv.meta.haskellPackages.hoogle
-          ];
-        });
+    inherit (pkgs-sam.haskell-lib) emacs-for-haskell haskell-pkg haskell-shell;
 
     name-generator = callPackage ./../src/haskell/name-generator/nix { };
     boardgamer = callPackage ./../src/haskell/boardgamer/nix { };
