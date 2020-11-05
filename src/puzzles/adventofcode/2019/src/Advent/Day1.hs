178910ebb759c7ba7c05f320df44c9e464e3832b
Samuel Rivas <samuelrivas@gmail.com>
2020-11-05 08:40:24 +0100

    hello world template

---
+++ src/puzzles/adventofcode/2019/src/Advent/Day1.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE DerivingStrategies  #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedLabels    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Advent.Day1 where
+
+import           System.IO.Advent (getInput)
+
+main :: IO ()
+main = putStrLn getInput "1"
