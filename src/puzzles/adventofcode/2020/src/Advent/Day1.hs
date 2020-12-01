b2b87b5a0fb13ff4e1736cd8c0d48e03c02cf697
Samuel Rivas <samuelrivas@gmail.com>
2020-12-01 10:57:32 +0100

    drity solution for day1

--- src/puzzles/adventofcode/2020/src/Advent/Day1.hs
+++ src/puzzles/adventofcode/2020/src/Advent/Day1.hs
@@ -7,10 +7,50 @@
 
 module Advent.Day1 where
 
-import           Data.Text.IO     (putStrLn)
-import           Prelude          hiding (putStrLn)
+import           Prelude          hiding (putStrLn, read)
+import qualified Prelude
 
-import           System.IO.Advent (getInput)
+import           Data.List        (find, tails)
+import           Data.Text        (Text, splitOn, unpack)
+import           Data.Text.IO     (putStrLn)
+
+import           System.IO.Advent as IOAdvent
+
+targetValue :: Int
+targetValue = 2020
+
+example :: [Int]
+example = [1721, 979, 366, 299, 675, 1456]
+
+-- TODO: Move read :: Text -> a to our own prelude
+read :: Read a => Text -> a
+read = Prelude.read . unpack
+
+-- TODO :: Move splitOn read to Advent
+getInput :: IO [Int]
+getInput =  fmap read . init . splitOn "\n" <$> IOAdvent.getInput "1"
+
+pairs :: [Int] -> [(Int, Int)]
+pairs l = zip l l
+
+-- TODO: Generalise to traversable
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _  = return []
+combinations n xs = do
+  y:xs' <- tails xs
+  ys <- combinations (n-1) xs'
+  return (y:ys)
+
+-- TODO: Prettyfy with a lens fold
+solve1 :: [Int] -> Maybe Int
+solve1 = fmap product <$> find ((== targetValue) . sum) . combinations 2
+
+-- TODO: Prettyfy with a lens fold
+solve2 :: [Int] -> Maybe Int
+solve2 = fmap product <$> find ((== targetValue) . sum) . combinations 3
 
 main :: IO ()
-main = getInput "1" >>= putStrLn
+main =
+  do
+    putStrLn "NA"
+    putStrLn "NA"
