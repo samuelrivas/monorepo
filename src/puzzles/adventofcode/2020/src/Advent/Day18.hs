d10d70d2a938af1d528fd90f7602a97d0eeb89b5
Samuel Rivas <samuelrivas@gmail.com>
2020-12-18 08:38:10 +0100

    day 18, part 2

--- src/puzzles/adventofcode/2020/src/Advent/Day18.hs
+++ src/puzzles/adventofcode/2020/src/Advent/Day18.hs
@@ -26,14 +26,14 @@ import qualified System.IO.Advent as IOAdvent
 data Tok = Digit Int
     | Open
     | Close
-    | Operand Char
+    | Operator Char
     deriving stock (Eq)
 
 instance Perlude.Show Tok where
-  show Open        = "("
-  show Close       = ")"
-  show (Operand c) = Prelude.show c
-  show (Digit d)   = Prelude.show d
+  show Open         = "("
+  show Close        = ")"
+  show (Operator c) = Prelude.show c
+  show (Digit d)    = Prelude.show d
 
 example :: Text
 example = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"
@@ -52,19 +52,29 @@ charToTok ')' = Close
 charToTok c =
   if isDigit c
   then Digit $ fromEnum c - fromEnum '0'
-  else Operand c
+  else Operator c
 
-precedence :: Tok -> Tok -> Bool
-precedence Open _ = False
-precedence _ _    = True
+precedence1 :: Tok -> Tok -> Bool
+precedence1 _ Open = False
+precedence1 _ _    = True
 
--- TODO: This is ugly, make it readable
+precedence2 :: Tok -> Tok -> Bool
+precedence2 _ Open                        = False
+precedence2 (Operator '+') (Operator '*') = False
+precedence2 _ _                           = True
+
 toPostfix :: [Tok] -> [Tok]
-toPostfix expr =
+toPostfix = generalisedToPostfix precedence1
+
+-- TODO: This is ugly and unreadable, make it pretty and readable
+
+-- a `precedenceF` b returns whether a has lower precedence than b
+generalisedToPostfix :: (Tok -> Tok -> Bool) -> [Tok] -> [Tok]
+generalisedToPostfix precedenceF expr =
   let
     f (stack, acc) digit@(Digit _) = (stack, acc ++ [digit])
-    f (stack, acc) op@(Operand _) =
-      (op : dropWhile (/= Open) stack, acc ++ takeWhile (/= Open) stack)
+    f (stack, acc) op@(Operator _) =
+      (op : dropWhile (precedenceF op) stack, acc ++ takeWhile (precedenceF op) stack)
     f (stack, acc) Close =
       (tail . dropWhile (/= Open) $ stack, acc ++ takeWhile (/= Open) stack)
     f (stack, acc) Open = (Open : stack, acc)
@@ -76,19 +86,22 @@ toPostfix expr =
 eval :: [Tok] -> Int
 eval =
   let
-    f stack (Digit d)                = d : stack
-    f (a : b : stack) op@(Operand _) = operate op a b : stack
-    f _ _                            = undefined
+    f stack (Digit d)                 = d : stack
+    f (a : b : stack) op@(Operator _) = operate op a b : stack
+    f _ _                             = undefined
   in
     head . foldl' f []
 
 operate :: Tok -> Int -> Int -> Int
-operate (Operand '+') = (+)
-operate (Operand '*') = (*)
-operate _             = undefined
+operate (Operator '+') = (+)
+operate (Operator '*') = (*)
+operate _              = undefined
 
 solution1 :: [[Tok]] -> Int
 solution1 = sum . fmap (eval . toPostfix)
+
+solution2 :: [[Tok]] -> Int
+solution2 = sum . fmap (eval . generalisedToPostfix precedence2)
 
 main :: IO ()
 main = do
@@ -98,4 +111,4 @@ main = do
   print . solution1 $ input
 
   putStr "Solution 2: "
-  print $ "NA"
+  print . solution2 $ input
