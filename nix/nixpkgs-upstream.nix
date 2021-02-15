39095c4314ffe62eb398bcb9a75f4d984209fc1b
Samuel Rivas <samuelrivas@gmail.com>
2021-02-15 14:25:05 +0100

    update nixpkgs sha
    Stay in 20.03 since haskellPackages are very broken after forcible update to
    cabal 3

--- nix/nixpkgs-upstream.nix
+++ nix/nixpkgs-upstream.nix
@@ -1,7 +1,7 @@
 let
-  version = "bb9da1323ebe39ff98a8412b963078a3a2bd2687";
+  version = "929768261a3ede470eafb58d5b819e1a848aa8bf";
   pkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${version}.tar.gz";
-    sha256 = "0979wgymadgmnyi0qh8r34gn5sfqi707mmpxzp2sj3jxb2alywxy";
+    sha256 = "0zi54vbfi6i6i5hdd4v0l144y1c8rg6hq6818jjbbcnm182ygyfa";
   };
 in import pkgs
