aecee9292c5bdf04d8b59f3917edbfe987e0d462
Samuel Rivas <samuelrivas@gmail.com>
2021-12-03 18:11:33 +0100

    another attempt for a stateful solve function that doesn't suck

--- src/puzzles/adventofcode/2021/src/Advent/Templib.hs
+++ src/puzzles/adventofcode/2021/src/Advent/Templib.hs
@@ -16,12 +16,17 @@ module Advent.Templib (
   ) where
 import           Perlude
 
-import           Control.Applicative  ((<|>))
-import           Data.Foldable        (foldl')
-import           Data.Functor         (($>))
-import           Data.List            (tails)
-import           Text.Parsec          (char, many1, sepEndBy)
-import           Text.Parsec.Parselib (Parser, literal)
+import           Control.Applicative   ((<|>))
+import           Control.Monad.RWS.CPS (RWST, evalRWST)
+import           Control.Monad.Reader  (MonadReader)
+import           Control.Monad.State   (MonadState)
+import           Data.Advent           (Day)
+import           Data.Foldable         (foldl')
+import           Data.Functor          (($>))
+import           Data.List             (tails)
+import           System.IO.Advent      (getParsedInput)
+import           Text.Parsec           (char, many1, sepEndBy)
+import           Text.Parsec.Parselib  (Parser, literal)
 
 -- TODO use this in day 2 or delete
 conv :: ([a] -> b) -> [a] -> [b]
@@ -40,3 +45,26 @@ bit = (literal "1" $> True) <|> (literal "0" $> False)
 
 bitString :: Parser [Bool]
 bitString = many1 bit
+
+-- Advent templates
+
+solveM ::
+  MonadFail m =>
+  MonadIO m =>
+  Show a =>
+  Show b =>
+  Day -> Parser input ->
+  RWST input () Text m a ->
+  RWST input () Text m b ->
+  m ()
+solveM day parser solver1 solver2 = do
+  input <- getParsedInput day parser
+
+  let solutions = (,) <$> solver1 <*> solver2
+  ((solution1, solution2), ()) <- evalRWST solutions input ""
+
+  putStr "Solution 1: "
+  print solution1
+
+  putStr "Solution 2: "
+  print solution2
