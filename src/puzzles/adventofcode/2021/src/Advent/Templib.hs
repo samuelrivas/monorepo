d59806f28353d12f617e6fb3ee4d7a28088ba2dd
Samuel Rivas <samuelrivas@gmail.com>
2021-12-03 14:31:10 +0100

    add a few common parsers

--- src/puzzles/adventofcode/2021/src/Advent/Templib.hs
+++ src/puzzles/adventofcode/2021/src/Advent/Templib.hs
@@ -9,15 +9,19 @@
 
 module Advent.Templib (
   binToDec,
+  bitString,
+  bit,
   conv,
   linesOf
   ) where
 import           Perlude
 
+import           Control.Applicative  ((<|>))
 import           Data.Foldable        (foldl')
+import           Data.Functor         (($>))
 import           Data.List            (tails)
-import           Text.Parsec          (char, sepEndBy)
-import           Text.Parsec.Parselib (Parser)
+import           Text.Parsec          (char, many1, sepEndBy)
+import           Text.Parsec.Parselib (Parser, literal)
 
 -- TODO use this in day 2 or delete
 conv :: ([a] -> b) -> [a] -> [b]
@@ -30,3 +34,9 @@ binToDec = foldl' (\acc b -> fromEnum b + 2*acc) 0
 
 linesOf :: Parser a -> Parser [a]
 linesOf p = p `sepEndBy` char '\n'
+
+bit :: Parser Bool
+bit = (literal "1" $> True) <|> (literal "0" $> False)
+
+bitString :: Parser [Bool]
+bitString = many1 bit
