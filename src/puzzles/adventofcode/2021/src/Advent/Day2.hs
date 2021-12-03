dd62bb64695f299241e70a84b4a139b383a51573
Samuel Rivas <samuelrivas@gmail.com>
2021-12-03 14:34:00 +0100

    not maximum prettiness, but prettier at least

--- src/puzzles/adventofcode/2021/src/Advent/Day2.hs
+++ src/puzzles/adventofcode/2021/src/Advent/Day2.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wall #-}
+
 {-# LANGUAGE DerivingStrategies  #-}
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE NoImplicitPrelude   #-}
@@ -10,16 +12,24 @@ module Advent.Day2 where
 
 import           Perlude
 
-import           Control.Lens         (each, sumOf)
+import           Control.Lens         (_1, _2, both, over, productOf)
+import           Control.Monad        (foldM)
+import           Control.Monad.State  (MonadState, evalState, get, modify)
 import           Data.Advent          (Day (..))
-import           Data.Bidim           (Coord, plus)
+import           Data.Bidim           (Coord)
 import           Data.Foldable        (foldl')
+import           Data.Functor         (($>))
 import           System.IO.Advent     (getInput, solve)
-import           Text.Parsec          (char, sepEndBy, (<|>))
-import           Text.Parsec.Advent   (listOfNum)
+import           Text.Parsec          ((<|>))
 import           Text.Parsec.Parselib (Parser, digitsAsNum, literal)
 
 import           Advent.Templib       (linesOf)
+
+data Instruction =
+  Forward Int
+  | Up Int
+  | Down Int
+  deriving stock (Show, Eq)
 
 day :: Day
 day = D2
@@ -27,34 +37,37 @@ day = D2
 rawInput :: IO Text
 rawInput = getInput day
 
-parser :: Parser [Coord]
+parser :: Parser [Instruction]
 parser = linesOf instruction
 
-instruction :: Parser Coord
+instruction :: Parser Instruction
 instruction =
-  literal "forward " *> ((,0) <$> digitsAsNum)
-  <|> literal "down " *> ((0,) <$> digitsAsNum)
-  <|> literal "up " *> ((0,) . negate  <$> digitsAsNum)
+  do
+    direction <- literal "forward " $> Forward
+                 <|> literal "down " $> Down
+                 <|> literal "up " $> Up
+    direction <$> digitsAsNum
+
+updatePos :: Instruction -> Coord -> Coord
+updatePos (Forward n) = over _1 (+n)
+updatePos (Up n)      = over _2 (subtract n)
+updatePos (Down n)    = over _2 (+n)
+
+updatePos2 :: MonadState Int m => Instruction -> Coord -> m Coord
+-- updatePos' (Forward n) = over _1 (+n)
+updatePos2 (Up n)      = (modify (subtract n) $>)
+updatePos2 (Down n)    = (modify (+ n) $>)
+updatePos2 (Forward n) = \coord -> do
+  aim <- get
+  pure $ over _1 (+n) . over _2 (+ n*aim) $ coord
 
 -- TODO I am pretty sure there is a prettier lens operation lurking around here.
--- TODO Coord really needs to be an instance of Sum, Num and perhaps Product
-solver1 :: [Coord] -> Int
-solver1 l =
-  let (x, y) = foldl' plus (0,0) l
-  in x * y
+solver1 :: [Instruction] -> Int
+solver1 = productOf both . foldl' (flip updatePos) (0,0)
 
--- TODO This is rather ugly, probably we should not be using Coord but rather a
--- sum type for the instructions
-solver2 :: [Coord] -> Int
-solver2 l =
-  let
-    f (x,y,aim) (forward,v)
-      | forward /= 0 = (x + forward, y + aim * forward, aim)
-      | otherwise = (x, y, aim +v)
-    (x, y, _) = foldl' f (0,0,0) l
-  in
-    x * y
+-- TODO Can we just lens into the state and eval after that?
+solver2 :: [Instruction] -> Int
+solver2 l = productOf both $ evalState (foldM (flip updatePos2) (0, 0) l) 0
 
 main :: IO ()
 main = solve day parser solver1 solver2
-
