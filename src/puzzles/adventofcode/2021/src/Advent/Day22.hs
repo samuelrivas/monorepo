fc5610c43ca819ed1d2a81625ee64839fce948d6
Samuel Rivas <samuelrivas@gmail.com>
2021-12-24 10:40:09 +0100

    some progrees day 22
    We can now union two cubes into a list of non overlapping cubes

--- src/puzzles/adventofcode/2021/src/Advent/Day22.hs
+++ src/puzzles/adventofcode/2021/src/Advent/Day22.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE NoImplicitPrelude   #-}
 {-# LANGUAGE OverloadedLabels    #-}
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections       #-}
 {-# LANGUAGE TypeOperators       #-}
@@ -17,10 +18,10 @@ import           Perlude
 import           Advent.Templib                  (linesOf)
 
 import           Control.Applicative             ((<|>))
-import           Control.Lens                    (Each (each), Getting, Prism',
-                                                  _1, _2, _3, _Just, both,
-                                                  preview, prism, sumOf,
-                                                  toListOf, view)
+import           Control.Lens                    (Each (each), Getting, Lens',
+                                                  Prism', _1, _2, _3, _Just,
+                                                  both, preview, prism, set,
+                                                  sumOf, toListOf, view)
 import           Control.Monad.State             (MonadState (get), evalState,
                                                   modify)
 import           Data.Advent                     (Day (..))
@@ -29,10 +30,12 @@ import           Data.Functor                    (($>))
 import           Data.Generics.Labels            ()
 import           Data.HashSet                    (HashSet)
 import qualified Data.HashSet                    as HashSet
-import           Data.List                       (foldl1', sort)
+import           Data.Ix                         (inRange)
+import           Data.List                       (foldl1', nub, sort)
 import           Data.Maybe                      (fromJust)
 import           Data.Text                       (intercalate)
 import           Distribution.Types.VersionRange (VersionRangeF (VersionRangeParensF))
+import           Numeric.Lens                    (subtracting)
 import qualified Prelude
 import           System.IO.Advent                (getInput, getParsedInput,
                                                   solve)
@@ -118,6 +121,85 @@ limitRange x y =
     y' = if y > 50 then 50 else y
   in
     [x'..y']
+
+-- coversSegment a b is true if b overlaps in any way with a
+coversSegment :: (Int, Int) -> (Int, Int) -> Bool
+coversSegment (x1, x2) (y1, y2) =
+  (x1 <= y1 && y1 <= x2)
+  || (x1 <= y2 && y2 <= x2)
+  || (y1 <= x1 && x2 <= y2)
+
+-- Projects a cube on a single dimension, provided by an accessor
+projectCube :: Lens' (Int, Int, Int) Int -> (Coord, Coord) -> (Int, Int)
+projectCube accessor cube =
+  (view (_1 . accessor) cube, view (_2 . accessor) cube) :: (Int, Int)
+
+-- coversCube a b is true if b overlaps in any way with a
+coversCube :: (Coord, Coord) -> (Coord, Coord) -> Bool
+coversCube  = allProjections coversSegment
+
+allProjections ::
+  ((Int, Int) -> (Int, Int) -> Bool) -> (Coord, Coord) -> (Coord, Coord) -> Bool
+allProjections f a b =
+  f (projectCube _1 a) (projectCube _1 b)
+  && f (projectCube _2 a) (projectCube _2 b)
+  && f (projectCube _3 a) (projectCube _3 b)
+
+-- TODO This reeks as something that can be done in a simpler way
+--
+-- splitSegment a b cuts b in chunks so that all of them are either fully inside a or fully outside a
+splitSegment :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
+splitSegment splitter@(a1, a2) segment@(b1, b2)
+  | a2 < b1 || b2 < a1 = [segment] -- disjoint
+  | a1 <= b1 && b2 <= a2 = [segment] -- splitter contains segment
+  | b1 < a1 && a2 < b2 = [(b1, a1 - 1), splitter, (a2 + 1, b2)] -- segment cotains splitter
+  | a2 < b2 = [(b1, a2), (a2 + 1, b2)] -- spiltter contains left end of segment
+  | a1 > b1 = [(b1, a1 - 1), (a1, b2)] -- splitter contains right end of segment
+  | otherwise = error "unexpected segments"
+
+-- Same as breakBy, but across a dimension set by the lens
+breakByAcross ::
+  Lens' (Int, Int, Int) Int -> (Int, Int) -> (Coord, Coord)
+  -> [(Coord, Coord)]
+breakByAcross accessor splitter cube =
+  let
+    segments = splitSegment splitter (projectCube accessor cube)
+    cubify (a, b) = set (_1 . accessor) a . set (_2 . accessor) b $  cube
+  in
+    cubify <$> segments
+
+countCells :: (Coord, Coord) -> Int
+countCells ((x, y, z), (x', y', z')) = (x' - x + 1) * (y' - y + 1) * (z' - z + 1)
+
+-- breakBy a b returns b exploded so that is composed of boxes that don't cross
+-- any edge of a
+breakBy :: (Coord, Coord) -> (Coord, Coord) -> [(Coord, Coord)]
+breakBy a b =
+  let
+    breakByDimmension (accessor  :: Lens' (Int, Int, Int) Int)
+      = breakByAcross accessor (projectCube accessor a)
+  in
+    breakByDimmension _1 b
+  >>= breakByDimmension _2
+  >>= breakByDimmension _3
+
+-- Joins two cubes into a list of non overlapping cubes
+union :: (Coord, Coord) -> (Coord, Coord) -> [(Coord, Coord)]
+union a b =
+  let
+    splits = breakBy a b
+  in
+    a : filter (not . subcube a) splits
+
+-- subcube a b returns whether b is fully included in a
+subcube :: (Coord, Coord) -> (Coord, Coord) -> Bool
+subcube = allProjections subsegment
+
+-- subsegment a b returns whether b is fully included in a
+subsegment :: (Int, Int) -> (Int, Int) -> Bool
+subsegment (x1, x2) (x1', x2') =
+  x1 <= x1' && x1' <= x2
+  && x1 <= x2' && x2' <= x2
 
 runReboot :: [Instruction] -> HashSet (Coord)
 runReboot instructions =
