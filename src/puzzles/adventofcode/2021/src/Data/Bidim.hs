f846cfb357c09ce203dd7d8e1a6ce1fc320bafa4
Samuel Rivas <samuelrivas@gmail.com>
2022-01-16 10:09:10 +0100

    don't export lenses to fields that cannot be modified

--- src/puzzles/adventofcode/2021/src/Data/Bidim.hs
+++ src/puzzles/adventofcode/2021/src/Data/Bidim.hs
@@ -4,47 +4,61 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE OverloadedLabels   #-}
 {-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE RankNTypes         #-}
 {-# LANGUAGE TupleSections      #-}
 
 module Data.Bidim (
   Bidim,
   Coord,
-  boundaries,
   empty,
   insert,
   toMap,
   cross,
   fromText,
   plus,
-  showBidim
+  showBidim,
+  cell,
+  singleton,
+  coords,
+  boundaries
   ) where
 
 import           Prelude              hiding (concat)
 
-import           Control.Lens         (Lens', _1, _2, at, lens, over, set,
-                                       toListOf, traverse, view)
+import           Control.Lens         (Getter, Lens', _1, _2, at, lens, over,
+                                       set, to, view)
 import           Data.Foldable        (foldl')
 import           Data.Generics.Labels ()
 import qualified Data.Map             as Map
 import           Data.Map.Strict      (Map)
 import           Data.Text            (Text, concat, intercalate, unpack)
-import           GHC.Generics         (Generic)
 
 type Coord = (Int, Int)
 
--- TOD making this accessible as generic is problematic as one could use lenses
--- to modify either boundaries or the map, we should insted export a setter that
--- uses insert and getters for the fields
+-- We don't export a generic lens for this record, as the 'boundaries' field
+-- depends on 'toMap'
 data Bidim a = Bidim {
-  toMap      :: Map Coord a,
-  boundaries :: (Coord, Coord)
+  toMap        :: Map Coord a,
+  toBoundaries :: (Coord, Coord)
   } deriving stock (Show, Eq)
 
-map' :: Lens' (Bidim a) (Map Coord a)
-map' = lens toMap (\b m -> b { toMap = m })
+-- Do not export this lens, as the toMap field cannot be modified freely
+asMap' :: Lens' (Bidim a) (Map Coord a)
+asMap' = lens toMap (\b m -> b { toMap = m })
 
+-- Do not export this lens, as the toBoundaries field cannot be modified freely
 boundaries' :: Lens' (Bidim a) (Coord, Coord)
-boundaries' = lens boundaries (\b m -> b { boundaries = m })
+boundaries' = lens toBoundaries (\b m -> b { toBoundaries = m })
+
+boundaries :: Getter (Bidim a) (Coord, Coord)
+boundaries = to toBoundaries
+
+coords :: Getter (Bidim a) [Coord]
+coords = to (Map.keys . toMap)
+
+-- Getter for the value of the Bidim in a given position
+cell :: Coord -> Getter (Bidim a) (Maybe a)
+cell c = to . view $ (asMap' . at c)
 
 -- Better would be to wrap this and make it an instance of Num
 plus :: Coord -> Coord -> Coord
@@ -53,6 +67,11 @@ plus (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 empty :: Bidim a
 empty = Bidim Map.empty (maxCoord, minCoord)
 
+singleton :: Coord -> a -> Bidim a
+singleton c a = Bidim (Map.singleton c a) (c, c)
+
+-- TODO This is a bit of a hack, since we don't really need to start from empty
+-- ever
 maxCoord :: Coord
 maxCoord = (maxBound, maxBound)
 
@@ -69,7 +88,7 @@ cross coord = [
 
 insert :: Coord -> a -> Bidim a -> Bidim a
 insert c a =
-  over map' (Map.insert c a)
+  over asMap' (Map.insert c a)
   . over boundaries' (extendBoundary c)
 
 extendBoundary :: Coord -> (Coord, Coord) -> (Coord, Coord)
@@ -79,10 +98,10 @@ extendBoundary (x, y) ((minX, minY), (maxX, maxY)) =
 showBidim :: (Maybe a -> Text) -> Bidim a -> Text
 showBidim format bidim =
   let
-    ((minX, minY), (maxX, maxY)) = boundaries bidim
+    ((minX, minY), (maxX, maxY)) = toBoundaries bidim
     row y = (, y) <$> [minX..maxX]
     showCoord :: Coord -> Text
-    showCoord coord = format $ view (#toMap . at coord) bidim
+    showCoord coord = format $ view (asMap' . at coord) bidim
     printed :: Int -> Text
     printed y = concat (showCoord <$> row y)
   in
