7bbf12b8108e41a6d620cf4190db9b4c0e224ba8
Samuel Rivas <samuelrivas@gmail.com>
2022-03-12 09:13:15 +0100

    move perlude to haskell-lib-mk

--- src/haskell/perlude/nix/default.nix
+++ src/haskell/perlude/nix/default.nix
@@ -1,23 +1,13 @@
 {
-  haskell-pkg,
+  haskell-lib-pkg,
   haskellPackages,
 }:
 let
   haskell-libs = with haskellPackages; [
   ];
-in haskell-pkg {
+in haskell-lib-pkg {
   name = "perlude";
   src = ./../src;
 
   inherit haskellPackages haskell-libs;
-
-  extra-drv = rec {
-    makeFlags = "PREFIX=$out";
-    propagatedBuildInputs = haskell-libs;
-    installPhase = ''
-      make ${makeFlags} install
-    '';
-    # Silently required by ghcWithPackages, for some reason
-    isHaskellLibrary = true;
-  };
 }
