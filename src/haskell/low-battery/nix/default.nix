5ad536a2fab238e2d02dc84c62e52e93d4ad573f
Samuel Rivas <samuelrivas@gmail.com>
2022-04-29 17:47:37 +0200

    Some fixes in low-battery
      - libnotify was pulled from haskellPackages now, so notify-send was not part
      of it
      - we need to build the script not the haskell binary. I assume it was set to
      the script for sandboxing purposes???
      - dbus configuration changes as I have a different user id on this laptop
    
      But honestly, I don't know what I am doing here

--- src/haskell/low-battery/nix/default.nix
+++ src/haskell/low-battery/nix/default.nix
@@ -2,7 +2,7 @@
   acpi,
   haskell-pkg,
   haskellPackages,
-  libnotify,
+  pkgs, # we get libnotify from here, if we write libnotify directly we get a haskell library
   writeShellScriptBin,
 }:
 let
@@ -17,9 +17,13 @@ let
     ];
     inherit haskellPackages;
   };
+  # FIXME The DBUS address should not be hardcoded...
   script = writeShellScriptBin "low-battery-notify" ''
-    export PATH=${libnotify}/bin:${acpi}/bin
-    export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1002/bus
+    export PATH=${pkgs.libnotify}/bin:${acpi}/bin
+    export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
     ${binary}/bin/low-battery-check
   '';
-in binary
+  # output binary instead of script if you want to sandbox this
+  # TODO fix this guacamole
+# in binary
+in script
