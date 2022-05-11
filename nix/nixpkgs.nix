b6af5c196f12919dc44a88bf8830739598c0620a
Samuel Rivas <samuelrivas@gmail.com>
2022-05-11 22:08:11 +0200

    update to latest 21.11

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 let
-  version = "e7d63bd0d50df412f5a1d8acfa3caae75522e347";
+  version = "aa2f845096f72dde4ad0c168eeec387cbd2eae04";
   pkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${version}.tar.gz";
-    sha256 = "sha256:132pc4f9ixisyv4117p2jirmlyl6sd76bfaz33rhlcwakg7bhjm7";
+    sha256 = "sha256:0l732ci2g78pcgk9kqn6c18h4j47dhp1dys52cmqhzm4pyi6dl0z";
   };
 in import pkgs
