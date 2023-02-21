f88f4c0fb1f772b5fb479fdc965f85b2a115f2d9
Samuel Rivas <samuelrivas@gmail.com>
2023-02-21 07:13:56 +0100

    semi working flakes structure

--- flake.nix
+++ flake.nix
@@ -4,10 +4,23 @@
   inputs = {
     nixpkgs-upstream.url = github:NixOS/nixpkgs;
     nixpkgs-stable.url = github:NixOS/nixpkgs/nixos-22.11;
-    nixpkgs-patched.url = github:samuelrivas/nixpkgs;
+    nixpkgs-sam.url = github:samuelrivas/nixpkgs;
   };
-  outputs = { self, nixpkgs-stable, nixpkgs-upstream, nixpkgs-patched }: {
-
-    nixpkgs-upstream.x86_64-linux.hello = nixpkgs-stable.legacyPackages.x86_64-linux.hello;
-  };
+  outputs = { self, nixpkgs-stable, nixpkgs-upstream, nixpkgs-sam }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      lib = nixpkgs-stable.lib;
+      forAllSystems = lib.genAttrs supportedSystems;
+      pkgs-sam = nixpkgs: {
+        udp-cat = with nixpkgs;
+          callPackage ./nix/pkgs/applications/networking/tools/udp-cat { };
+      };
+    in rec {
+      overlays.default = final: prev: pkgs-sam prev;
+      packages = forAllSystems (system:
+        let all-pkgs = pkgs-sam (import nixpkgs-stable { inherit system; });
+        in all-pkgs // { default = all-pkgs.udp-cat; }
+      );
+      # defaultPackage = forAllSystems (system: packages.${system}.udp-cat);
+    };
 }
