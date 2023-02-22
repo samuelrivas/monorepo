022aa5baf523597a8ea4df821f65b600e8794aaf
Samuel Rivas <samuelrivas@gmail.com>
2023-02-22 06:29:35 +0100

    comments and indent

--- nix/pkgs-sam.nix
+++ nix/pkgs-sam.nix
@@ -1,3 +1,9 @@
+# We load this function in flake.nix to overlay all our packages over the
+# nixpkgs collection.
+#
+# Note that this is incomplete, several packages are only available via the
+# legacy nix/default.nix. We'll keep migrating those to here and eventually
+# deprecate nix/default.nix.
 nixpkgs:
 let
   builders = nixpkgs.callPackage ./lib/build-support/builders.nix { };
@@ -9,7 +15,8 @@ let
     # Emacs stuff
     # =============
     # aspell needs to be configured to find the dictionaries
-    aspell-wrapped = callPackage ./pkgs/development/libraries/aspell-wrapped { };
+    aspell-wrapped =
+      callPackage ./pkgs/development/libraries/aspell-wrapped { };
 
     # Shell-scripts
     # =============
@@ -19,16 +26,15 @@ let
 
     sh-lib = callPackage ./../src/shell/sh-lib/nix { };
 
-    commit-hook-ticket-prefix = callPackage ./../src/shell/commit-hook-ticket-prefix/nix { };
+    commit-hook-ticket-prefix =
+      callPackage ./../src/shell/commit-hook-ticket-prefix/nix { };
 
     # C++ stuff
     # =========
     reservoir = callPackage ./../src/c++/reservoir/nix { };
 
     monte-carlo = callPackage ./../src/c++/monte-carlo/nix { };
-    algos-n-fun = callPackage ./../src/c++/algos-n-fun/nix {
-      # inherit (nixpkgs) rapidcheck;
-    };
+    algos-n-fun = callPackage ./../src/c++/algos-n-fun/nix { };
     finndb = callPackage ./../src/c++/finndb/nix { };
     graphlib = callPackage ./../src/c++/graphlib/nix { };
     rndlib = callPackage ./../src/c++/rndlib/nix { };
