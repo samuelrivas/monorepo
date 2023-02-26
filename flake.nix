7f27e86d489b10425ffdc958c98c2c3edca6fb0a
Samuel Rivas <samuelrivas@gmail.com>
2023-02-26 07:06:37 +0100

    Better flake structure
     - We add all our packages and libs in their own namespace (though we also
       inject the packages into the global namespace for now
     - We provide a default derivation that builds all our derivations (little ulgy,
       but is a way to support a similar functionality than nix build on the top
       level)
     - We provide a clean override, and a list of all our packages as packages

--- flake.nix
+++ flake.nix
@@ -11,19 +11,20 @@
       supported-systems = [ "x86_64-linux" ];
       lib = nixpkgs-stable.lib;
       for-all-systems = lib.genAttrs supported-systems;
-      # pkgs-sam = nixpkgs: {
-      #   udp-cat = with nixpkgs;
-      #     callPackage ./nix/pkgs/applications/networking/tools/udp-cat { };
-      # };
-      pkgs-sam = import ./nix/pkgs-sam.nix;
     in rec {
-      overlays.default = final: prev: (prev // pkgs-sam prev);
+      overlays.default = import ./nix/pkgs-sam.nix;
 
       # TODO: use the overlay here, but you will need to have legacyPackages to
       # avoid cluttering this with all nixpkgs
       packages = for-all-systems (system:
-        let all-pkgs = pkgs-sam (import nixpkgs-stable { inherit system; });
-        in all-pkgs // { default = all-pkgs.udp-cat; }
+        let
+          pkgs-stable = import nixpkgs-stable {
+            inherit system;
+            overlays = [ self.overlays.default ];
+            config = { };
+          };
+        in pkgs-stable.derivations-sam
+           // { default = pkgs-stable.derivations-sam.all-pkgs-sam; }
       );
     };
 }
