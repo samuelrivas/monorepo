3d62ed96b8e255af50982c60c44108a426ed6906
Samuel Rivas <samuelrivas@gmail.com>
2023-02-26 07:06:37 +0100

    Better flake structure
     - We add all our packages and libs in their own namespace (though we also
       inject the packages into the global namespace for now
     - We provide a default derivation that builds all our derivations (little ulgy,
       but is a way to support a similar functionality than nix build on the top
       level)
     - We provide a clean override, and a list of all our packages as packages

--- nix/pkgs-sam.nix
+++ nix/pkgs-sam.nix
@@ -4,42 +4,59 @@
 # Note that this is incomplete, several packages are only available via the
 # legacy nix/default.nix. We'll keep migrating those to here and eventually
 # deprecate nix/default.nix.
-nixpkgs:
+final: prev:
 let
-  builders = nixpkgs.callPackage ./lib/build-support/builders.nix { };
+  builders = final.callPackage ./lib/build-support/builders.nix { };
   derivation-helpers = import ./lib/derivation-helpers.nix;
-  callPackage = nixpkgs.lib.callPackageWith
-    (nixpkgs // builders // derivation-helpers // pkgs-sam);
-  pkgs-sam = {
+  libs-sam = { inherit builders derivation-helpers; };
+  callPackage = final.lib.callPackageWith
+    (final.pkgs // builders // derivation-helpers);
+  pkgs = {
 
-    # Emacs stuff
-    # =============
-    # aspell needs to be configured to find the dictionaries
-    aspell-wrapped =
-      callPackage ./pkgs/development/libraries/aspell-wrapped { };
+    # We keep our libraries in a separate attrset and pass them with
+    # callPackageWith instead of polluting the top level namespace with them. We
+    # do add all our pakcages to the top level namespace though
+    inherit libs-sam;
 
-    # Shell-scripts
-    # =============
-    assorted-scripts = callPackage ./../src/shell/assorted-scripts/nix {
-      inherit (nixpkgs.xorg) xbacklight xrandr xset;
+    derivations-sam = {
+      # Emacs stuff
+      # =============
+      # aspell needs to be configured to find the dictionaries
+      aspell-wrapped =
+        callPackage ./pkgs/development/libraries/aspell-wrapped { };
+
+      # Shell-scripts
+      # =============
+      assorted-scripts = callPackage ./../src/shell/assorted-scripts/nix {
+        inherit (final.xorg) xbacklight xrandr xset;
+      };
+
+      sh-lib = callPackage ./../src/shell/sh-lib/nix { };
+
+      commit-hook-ticket-prefix =
+        callPackage ./../src/shell/commit-hook-ticket-prefix/nix { };
+
+      # C++ stuff
+      # =========
+      reservoir = callPackage ./../src/c++/reservoir/nix { };
+
+      monte-carlo = callPackage ./../src/c++/monte-carlo/nix { };
+      algos-n-fun = callPackage ./../src/c++/algos-n-fun/nix { };
+      finndb = callPackage ./../src/c++/finndb/nix { };
+      graphlib = callPackage ./../src/c++/graphlib/nix { };
+      rndlib = callPackage ./../src/c++/rndlib/nix { };
+      asyncq = callPackage ./../src/c++/asyncq/nix { };
+
+      udp-cat = final.callPackage ./pkgs/applications/networking/tools/udp-cat { };
     };
-
-    sh-lib = callPackage ./../src/shell/sh-lib/nix { };
-
-    commit-hook-ticket-prefix =
-      callPackage ./../src/shell/commit-hook-ticket-prefix/nix { };
-
-    # C++ stuff
-    # =========
-    reservoir = callPackage ./../src/c++/reservoir/nix { };
-
-    monte-carlo = callPackage ./../src/c++/monte-carlo/nix { };
-    algos-n-fun = callPackage ./../src/c++/algos-n-fun/nix { };
-    finndb = callPackage ./../src/c++/finndb/nix { };
-    graphlib = callPackage ./../src/c++/graphlib/nix { };
-    rndlib = callPackage ./../src/c++/rndlib/nix { };
-    asyncq = callPackage ./../src/c++/asyncq/nix { };
-
-    udp-cat = callPackage ./pkgs/applications/networking/tools/udp-cat { };
+    all-pkgs-sam = final.symlinkJoin {
+      name = "pkgs-sam";
+      paths = builtins.attrValues pkgs.derivations-sam;
+    };
   };
-in pkgs-sam
+in pkgs // pkgs.derivations-sam // {
+  # Add all-pkgs-sam here to the set with all derivations to avoid infinite
+  # recursion
+  derivations-sam = pkgs.derivations-sam // { inherit (pkgs) all-pkgs-sam; };
+}
+
