9f2c12184c254b7f73fd981dec77eaed0941a7f0
Samuel Rivas <samuelrivas@gmail.com>
2023-05-18 08:39:01 +0200

    Merge haskell-mk and haskell-lib-mk
    And use the new dev-shell implementation for both, including hoogle

--- nix/lib/haskell.nix
+++ nix/lib/haskell.nix
@@ -1,62 +1,27 @@
 {
   pkgs,
-}: {
-  # Create a haskell package with haskell-mk included and enough meta to create
-  # shell environments
-  #
+}:
   # We export Werror to fail the build by default (assuming that the package
   # Makefile does not do GHC-FLAGS := ..., but GHC-FLAGS += ...), but you can
   # unset that when running in a sandbox for quick iterations.
-  #
-  # FIXME: There is a fair amount of duplication between haskell-pkg and
-  # haskell-lib-pkg
-  haskell-pkg =
-    { name,
-      src,
-      haskell-libs,
-      extra-build-inputs ? [],
-      extra-drv ? { },
-    }:
-    let
-      ghc = pkgs.haskellPackages.ghcWithPackages (_: haskell-libs);
-      drv-args = {
-
-        inherit name src;
-        GHC-FLAGS = "-Werror";
-        buildInputs = [
-          ghc
-          pkgs.haskell-mk
-          pkgs.haskell-test-mk
-        ] ++ extra-build-inputs;
-
-        nativeBuildInputs = [ ];
-
-        installPhase = ''
-            mkdir -p $out/bin
-            cp ../build/bin/* $out/bin
-          '';
-
-      } // extra-drv;
-      drv = pkgs.stdenv.mkDerivation drv-args;
-      self =
-        drv // {
-          dev-shell = self.overrideAttrs (final: previous: {
-            nativeBuildInputs =
-              previous.nativeBuildInputs ++ [ pkgs.haskell-language-server ];
-          });
-        };
-      in self;
-
-  haskell-lib-pkg =
+let
+  haskell-template = is-lib:
     { extra-build-inputs ? [],
-      extra-native-build-inputs ? [],
       extra-drv ? { },
+      extra-native-build-inputs ? [],
       haskell-libs,
       name,
       src,
     }:
     let
       ghc = pkgs.haskellPackages.ghcWithPackages (_: haskell-libs);
+      install-bin = ''
+        mkdir -p $out/bin
+        cp ../build/bin/* $out/bin
+      '';
+      install-lib = ''
+        make PREFIX="$out" install
+      '';
       drv-args = {
 
         inherit name src;
@@ -66,17 +31,16 @@
 
         nativeBuildInputs = [
           ghc
-          pkgs.haskell-lib-mk
+          (if (is-lib) then pkgs.haskell-lib-mk else pkgs.haskell-mk)
           pkgs.haskell-test-mk
         ] ++ extra-native-build-inputs;
 
-        propagatedBuildInputs = haskell-libs;
-        installPhase = ''
-          make PREFIX="$out" install
-        '';
+        propagatedBuildInputs = if is-lib then haskell-libs else [];
+        installPhase = if is-lib then install-lib else install-bin;
 
-        # Silently required by ghcWithPackages, for some reason
-        isHaskellLibrary = true;
+        # ghcWithPackages needs this to be true for libraries, for non libraries
+        # it could be omitted
+        isHaskellLibrary = is-lib;
       } // extra-drv;
       drv = pkgs.stdenv.mkDerivation drv-args;
       dev-shell = drv.overrideAttrs (final: previous: {
@@ -92,6 +56,8 @@
                (pkgs.haskellPackages.ghcWithHoogle (_: haskell-libs))
              ];
       });
-    in
-      drv // { inherit dev-shell; };
+    in drv // { inherit dev-shell; };
+in {
+  haskell-pkg = haskell-template false;
+  haskell-lib-pkg = haskell-template true;
 }
