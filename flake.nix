a5dab9826f7e3ad93b704115962e25f41a4f2d0a
Samuel Rivas <samuelrivas@gmail.com>
2023-06-03 06:45:31 +0200

    Add flake-parts initial boiler

--- flake.nix
+++ flake.nix
@@ -2,43 +2,46 @@
   description = "Sam's monorepo derivation collection";
 
   inputs = {
-    # nixpkgs-upstream.url = github:NixOS/nixpkgs;
     nixpkgs-stable.url = github:NixOS/nixpkgs/nixos-22.11;
-    # nixpkgs-sam.url = github:samuelrivas/nixpkgs;
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = {
-    self,
-    nixpkgs-stable,
-    # nixpkgs-upstream,
-    # nixpkgs-sam
-  }:
-    let
-      supported-systems = [ "x86_64-linux" ];
-      lib = nixpkgs-stable.lib;
-      for-all-systems = lib.genAttrs supported-systems;
-    in rec {
-      overlays.default = import ./nix/pkgs-sam.nix;
+  outputs =
+    inputs@{
+      flake-parts,
+      nixpkgs-stable,
+      self,
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = let
+        supported-systems = [ "x86_64-linux" ];
+        lib = nixpkgs-stable.lib;
+        for-all-systems = lib.genAttrs supported-systems;
+      in rec {
+        overlays.default = import ./nix/pkgs-sam.nix;
 
-      # TODO: use the overlay here, but you will need to have legacyPackages to
-      # avoid cluttering this with all nixpkgs
-      packages = for-all-systems (system:
-        let
-          pkgs-stable = import nixpkgs-stable {
-            inherit system;
-            overlays = [ self.overlays.default ];
-            config = { };
-          };
-        in pkgs-stable.derivations-sam
-           // { default = pkgs-stable.derivations-sam.all-pkgs-sam; }
-      );
+        # TODO: use the overlay here, but you will need to have legacyPackages to
+        # avoid cluttering this with all nixpkgs
+        packages = for-all-systems (system:
+          let
+            pkgs-stable = import nixpkgs-stable {
+              inherit system;
+              overlays = [ self.overlays.default ];
+              config = { };
+            };
+          in pkgs-stable.derivations-sam
+             // { default = pkgs-stable.derivations-sam.all-pkgs-sam; }
+        );
 
-      devShells = for-all-systems (system:
-        builtins.mapAttrs
-          (name: value:
-            if builtins.hasAttr "dev-shell" packages.${system}.${name}
-            then packages.${system}.${name}.dev-shell
-            else packages.${system}.${name})
-          packages.${system}
-      );
+        devShells = for-all-systems (system:
+          builtins.mapAttrs
+            (name: value:
+              if builtins.hasAttr "dev-shell" packages.${system}.${name}
+              then packages.${system}.${name}.dev-shell
+              else packages.${system}.${name})
+            packages.${system}
+        );
+      };
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, system, lib, specialArgs, options } : { };
     };
 }
