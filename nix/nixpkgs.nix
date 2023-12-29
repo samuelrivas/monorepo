082e0141be5f4505f8e337d0ca66243417543945
Samuel Rivas <samuelrivas@gmail.com>
2023-12-29 16:23:18 +0100

    Delete files that are no longer in use.

--- nix/nixpkgs.nix
+++
@@ -1,9 +0,0 @@
-# Reference to the latest nixos-22.05
-let
-  version = "96e18717904dfedcd884541e5a92bf9ff632cf39";
-  pkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${version}.tar.gz";
-    sha256 = "sha256:0zw1851mia86xqxdf8jgy1c6fm5lqw4rncv7v2lwxar3vhpn6c78";
-  };
-in
-  import pkgs
