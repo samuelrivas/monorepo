9ac06d80765dcdfa32fb5ae2d6dd05c9f524fd17
Samuel Rivas <samuelrivas@gmail.com>
2024-04-13 08:16:16 +0200

    create outputs in let
    Instead of just in the in expression, because this gives us more flexibility

--- flake.nix
+++ flake.nix
@@ -12,8 +12,6 @@
     nixpkgs-stable,
     vscode-extensions,
   }: let
-    inherit (lib-sam.flake) instantiate-nixpkgs;
-
     lib-nixpkgs = nixpkgs-stable.lib;
     lib-sam = import ./nix/lib.nix {
       inherit lib-nixpkgs;
@@ -21,63 +19,67 @@
 
     supported-systems = ["x86_64-linux"];
     for-all-systems = lib-sam.flake.for-all-systems supported-systems;
-  in rec {
-    formatter =
-      for-all-systems (system:
-        nixpkgs-stable.legacyPackages.${system}.alejandra);
 
-    legacy.lib.sam = import ./nix/legacy/lib.nix;
+    inherit (lib-sam.flake) instantiate-nixpkgs;
+    outputs = rec {
+      formatter =
+        for-all-systems (system:
+          nixpkgs-stable.legacyPackages.${system}.alejandra);
 
-    lib.sam = lib-sam;
+      legacy.lib.sam = import ./nix/legacy/lib.nix;
 
-    # TODO Go over these input parameters and make sense of them according to The Principles
-    packages = for-all-systems (
-      system: let
-        lib-system = lib.sam.system {
-          packages-nixpkgs = nixpkgs-stable.legacyPackages.${system};
-          packages-sam = packages;
-        };
-        bundle-packages = lib-system.packages.bundle {name = "all-packages";};
-        instantiate-packages-sam = nixpkgs:
-          import ./nix/packages.nix {
-            legacy-lib = legacy.lib.sam;
-            lib = nixpkgs.lib;
-            system-lib = {
-              sam = lib.sam.system {
-                packages-nixpkgs = instantiate-nixpkgs nixpkgs-stable system;
-                packages-sam = packages.${system};
+      lib.sam = lib-sam;
+
+      # TODO Go over these input parameters and make sense of them according to The Principles
+      packages = for-all-systems (
+        system: let
+          lib-system = lib.sam.system {
+            packages-nixpkgs = nixpkgs-stable.legacyPackages.${system};
+            packages-sam = packages;
+          };
+          bundle-packages = lib-system.packages.bundle {name = "all-packages";};
+          instantiate-packages-sam = nixpkgs:
+            import ./nix/packages.nix {
+              legacy-lib = legacy.lib.sam;
+              lib = nixpkgs.lib;
+              system-lib = {
+                sam = lib.sam.system {
+                  packages-nixpkgs = instantiate-nixpkgs nixpkgs-stable system;
+                  packages-sam = packages.${system};
+                };
               };
+              inherit nixpkgs;
+              vscode-extensions = vscode-extensions.outputs.extensions.${system};
             };
-            inherit nixpkgs;
-            vscode-extensions = vscode-extensions.outputs.extensions.${system};
-          };
-        packages-sam-stable = instantiate-packages-sam (instantiate-nixpkgs nixpkgs-stable system);
-        packages-sam-22-11 = instantiate-packages-sam (instantiate-nixpkgs nixpkgs-22-11 system);
-        final-packages =
-          packages-sam-stable
+          packages-sam-stable = instantiate-packages-sam (instantiate-nixpkgs nixpkgs-stable system);
+          packages-sam-22-11 = instantiate-packages-sam (instantiate-nixpkgs nixpkgs-22-11 system);
+          final-packages =
+            packages-sam-stable
+            // {
+              # These don't build with nixpkgs-stable. We will be eventually fix
+              # them to avoid carrying old versions of nixpkgs around
+              adventofcode-2019 = packages-sam-22-11.adventofcode-2019;
+            };
+          # TODO Fix whatever broke in 2019
+          all-packages = bundle-packages (builtins.removeAttrs final-packages ["adventofcode-2019"]);
+        in
+          final-packages
           // {
-            # These don't build with nixpkgs-stable. We will be eventually fix
-            # them to avoid carrying old versions of nixpkgs around
-            adventofcode-2019 = packages-sam-22-11.adventofcode-2019;
-          };
-        # TODO Fix whatever broke in 2019
-        all-packages = bundle-packages (builtins.removeAttrs final-packages ["adventofcode-2019"]);
-      in
-        final-packages
-        // {
-          inherit all-packages;
-          default = all-packages;
-        }
-    );
+            inherit all-packages;
+            default = all-packages;
+          }
+      );
 
-    devShells = for-all-systems (
-      system:
-        builtins.mapAttrs
-        (name: value:
-          if builtins.hasAttr "dev-shell" packages.${system}.${name}
-          then packages.${system}.${name}.dev-shell
-          else packages.${system}.${name})
-        packages.${system}
-    );
-  };
+      devShells = for-all-systems (
+        system:
+          builtins.mapAttrs
+          (name: value:
+            if builtins.hasAttr "dev-shell" packages.${system}.${name}
+            then packages.${system}.${name}.dev-shell
+            else packages.${system}.${name})
+          packages.${system}
+      );
+    };
+  in
+    outputs;
 }
