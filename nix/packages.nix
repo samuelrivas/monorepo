36294da8295f1923eff75a0f96f4cb0df391c3fe
Samuel Rivas <samuelrivas@gmail.com>
2024-05-11 07:29:42 +0200

    rename pkgs.derivations-sam to packages

--- nix/packages.nix
+++ nix/packages.nix
@@ -10,7 +10,7 @@
   derivation-helpers = legacy-lib.derivation-helpers;
   callPackage =
     lib-nixpkgs.callPackageWith
-    (nixpkgs // pkgs.derivations-sam // builders // derivation-helpers);
+    (nixpkgs // packages // builders // derivation-helpers);
   haskellPackages = nixpkgs.haskellPackages.override {
     overrides = h-pkgs: h-prev: let
       h-package = h-pkgs.callPackage;
@@ -38,110 +38,108 @@
     };
   };
 
-  pkgs = {
-    derivations-sam = {
-      # Emacs stuff
-      # =============
-      emacs-config = callPackage ./../src/elisp/emacs-config/nix {};
+  packages = {
+    # Emacs stuff
+    # =============
+    emacs-config = callPackage ./../src/elisp/emacs-config/nix {};
 
-      copilot = callPackage ./pkgs/applications/editors/copilot {
-        inherit
-          (nixpkgs.emacsPackages)
-          dash
-          editorconfig
-          s
-          trivialBuild
-          ;
-      };
-
-      # An emacs wrapper with the needed packages accessible
-      my-emacs = callPackage ./pkgs/applications/editors/my-emacs {
-        inherit
-          (nixpkgs.emacsPackages)
-          colorThemeSolarized
-          company
-          eglot
-          flycheck-haskell
-          groovy-mode
-          helm
-          helm-ls-git
-          helm-org
-          htmlize
-          markdown-mode
-          nix-mode
-          projectile
-          terraform-mode
-          yaml-mode
-          yasnippet
-          ;
-        inherit (nixpkgs.python3Packages) jedi-language-server;
-      };
-
-      # aspell needs to be configured to find the dictionaries
-      aspell-wrapped =
-        callPackage ./pkgs/development/libraries/aspell-wrapped {};
-
-      # VSC stuff
-      # =========
-      my-vscode = callPackage ./pkgs/applications/editors/my-vscode {
-        # TODO: pass marketplace directly instead of the whole extensions package
-        inherit (vscode-extensions) vscode-marketplace;
-        terraform = nixpkgs.terraform.withPlugins (p: [p.aws]);
-      };
-
-      # Haskell stuff
-      # =============
-      haskell-mk = callPackage ./../src/haskell/haskell-mk/nix {};
-      haskell-lib-mk = callPackage ./../src/haskell/haskell-lib-mk/nix {};
-      haskell-test-mk = callPackage ./../src/haskell/haskell-test-mk/nix {};
-
-      adventlib = haskellPackages.adventlib;
-      adventlib-old-1 = haskellPackages.adventlib-old-1;
-      boardgamer = haskellPackages.boardgamer;
-      boollib = haskellPackages.boollib;
-      clean-clocks = haskellPackages.clean-clocks;
-      hashcode-photoalbum = haskellPackages.hashcode-photoalbum;
-      low-battery = haskellPackages.low-battery;
-      mk-conf-file = haskellPackages.mk-conf-file;
-      monad-emit = haskellPackages.monad-emit;
-      name-generator = haskellPackages.name-generator;
-      onirim-helper = haskellPackages.onirim-helper;
-      parselib = haskellPackages.parselib;
-      perlude = haskellPackages.perlude;
-      searchlib = haskellPackages.searchlib;
-
-      # Shell-scripts
-      # =============
-      assorted-scripts = callPackage ./../src/shell/assorted-scripts/nix {
-        inherit (nixpkgs.xorg) xbacklight xrandr xset;
-      };
-
-      sh-lib = callPackage ./../src/shell/sh-lib/nix {};
-
-      commit-hook-ticket-prefix =
-        callPackage ./../src/shell/commit-hook-ticket-prefix/nix {};
-
-      sandbox = callPackage ./../src/shell/sandbox/nix {};
-
-      # C++ stuff
-      # =========
-      reservoir = callPackage ./../src/c++/reservoir/nix {};
-
-      monte-carlo = callPackage ./../src/c++/monte-carlo/nix {};
-      algos-n-fun = callPackage ./../src/c++/algos-n-fun/nix {};
-      finndb = callPackage ./../src/c++/finndb/nix {};
-      graphlib = callPackage ./../src/c++/graphlib/nix {};
-      rndlib = callPackage ./../src/c++/rndlib/nix {};
-      asyncq = callPackage ./../src/c++/asyncq/nix {};
-
-      udp-cat = callPackage ./pkgs/applications/networking/tools/udp-cat {};
-
-      # Contests, puzzles, etc
-      # ======================
-      adventofcode-2019 = haskellPackages.adventofcode-2019;
-      adventofcode-2020 = haskellPackages.adventofcode-2020;
-      adventofcode-2021 = haskellPackages.adventofcode-2021;
+    copilot = callPackage ./pkgs/applications/editors/copilot {
+      inherit
+        (nixpkgs.emacsPackages)
+        dash
+        editorconfig
+        s
+        trivialBuild
+        ;
     };
+
+    # An emacs wrapper with the needed packages accessible
+    my-emacs = callPackage ./pkgs/applications/editors/my-emacs {
+      inherit
+        (nixpkgs.emacsPackages)
+        colorThemeSolarized
+        company
+        eglot
+        flycheck-haskell
+        groovy-mode
+        helm
+        helm-ls-git
+        helm-org
+        htmlize
+        markdown-mode
+        nix-mode
+        projectile
+        terraform-mode
+        yaml-mode
+        yasnippet
+        ;
+      inherit (nixpkgs.python3Packages) jedi-language-server;
+    };
+
+    # aspell needs to be configured to find the dictionaries
+    aspell-wrapped =
+      callPackage ./pkgs/development/libraries/aspell-wrapped {};
+
+    # VSC stuff
+    # =========
+    my-vscode = callPackage ./pkgs/applications/editors/my-vscode {
+      # TODO: pass marketplace directly instead of the whole extensions package
+      inherit (vscode-extensions) vscode-marketplace;
+      terraform = nixpkgs.terraform.withPlugins (p: [p.aws]);
+    };
+
+    # Haskell stuff
+    # =============
+    haskell-mk = callPackage ./../src/haskell/haskell-mk/nix {};
+    haskell-lib-mk = callPackage ./../src/haskell/haskell-lib-mk/nix {};
+    haskell-test-mk = callPackage ./../src/haskell/haskell-test-mk/nix {};
+
+    adventlib = haskellPackages.adventlib;
+    adventlib-old-1 = haskellPackages.adventlib-old-1;
+    boardgamer = haskellPackages.boardgamer;
+    boollib = haskellPackages.boollib;
+    clean-clocks = haskellPackages.clean-clocks;
+    hashcode-photoalbum = haskellPackages.hashcode-photoalbum;
+    low-battery = haskellPackages.low-battery;
+    mk-conf-file = haskellPackages.mk-conf-file;
+    monad-emit = haskellPackages.monad-emit;
+    name-generator = haskellPackages.name-generator;
+    onirim-helper = haskellPackages.onirim-helper;
+    parselib = haskellPackages.parselib;
+    perlude = haskellPackages.perlude;
+    searchlib = haskellPackages.searchlib;
+
+    # Shell-scripts
+    # =============
+    assorted-scripts = callPackage ./../src/shell/assorted-scripts/nix {
+      inherit (nixpkgs.xorg) xbacklight xrandr xset;
+    };
+
+    sh-lib = callPackage ./../src/shell/sh-lib/nix {};
+
+    commit-hook-ticket-prefix =
+      callPackage ./../src/shell/commit-hook-ticket-prefix/nix {};
+
+    sandbox = callPackage ./../src/shell/sandbox/nix {};
+
+    # C++ stuff
+    # =========
+    reservoir = callPackage ./../src/c++/reservoir/nix {};
+
+    monte-carlo = callPackage ./../src/c++/monte-carlo/nix {};
+    algos-n-fun = callPackage ./../src/c++/algos-n-fun/nix {};
+    finndb = callPackage ./../src/c++/finndb/nix {};
+    graphlib = callPackage ./../src/c++/graphlib/nix {};
+    rndlib = callPackage ./../src/c++/rndlib/nix {};
+    asyncq = callPackage ./../src/c++/asyncq/nix {};
+
+    udp-cat = callPackage ./pkgs/applications/networking/tools/udp-cat {};
+
+    # Contests, puzzles, etc
+    # ======================
+    adventofcode-2019 = haskellPackages.adventofcode-2019;
+    adventofcode-2020 = haskellPackages.adventofcode-2020;
+    adventofcode-2021 = haskellPackages.adventofcode-2021;
   };
 in
-  pkgs.derivations-sam
+  packages
