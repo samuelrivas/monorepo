0bea0377135a03a3180c4e33409c336ec1be12d1
Samuel Rivas <samuelrivas@gmail.com>
2024-07-23 17:29:57 +0200

    formatting

--- src/haskell/haskell-lib-mk/nix/default.nix
+++ src/haskell/haskell-lib-mk/nix/default.nix
@@ -1,31 +1,30 @@
 {
   mk-conf-file,
   stdenv,
-}:
-let
+}: let
   arch = builtins.replaceStrings ["darwin"] ["osx"] stdenv.targetPlatform.system;
 in
-stdenv.mkDerivation {
-  name = "haskell-lib-mk";
-  src = ./../src;
-  propagatedBuildInputs = [mk-conf-file];
-  installPhase = ''
-    mkdir -p "$out/lib"
-    mkdir -p "$out/nix-support"
-    cp lib/build-haskell-lib.mk $out/lib
+  stdenv.mkDerivation {
+    name = "haskell-lib-mk";
+    src = ./../src;
+    propagatedBuildInputs = [mk-conf-file];
+    installPhase = ''
+      mkdir -p "$out/lib"
+      mkdir -p "$out/nix-support"
+      cp lib/build-haskell-lib.mk $out/lib
 
-    cat > $out/nix-support/setup-hook <<EOF
-    addHaskellLibMkPath() {
-      export HASKELL_LIB_MK="$out/lib/build-haskell-lib.mk"
-    }
+      cat > $out/nix-support/setup-hook <<EOF
+      addHaskellLibMkPath() {
+        export HASKELL_LIB_MK="$out/lib/build-haskell-lib.mk"
+      }
 
-    addArchInfo() {
-      export ARCH="${arch}"
-      export SHARED_LIB_EXTENSION="${stdenv.targetPlatform.extensions.sharedLibrary}"
-    }
+      addArchInfo() {
+        export ARCH="${arch}"
+        export SHARED_LIB_EXTENSION="${stdenv.targetPlatform.extensions.sharedLibrary}"
+      }
 
-    addEnvHooks "$hostOffset" addHaskellLibMkPath
-    addEnvHooks "$hostOffset" addArchInfo
-    EOF
-  '';
-}
+      addEnvHooks "$hostOffset" addHaskellLibMkPath
+      addEnvHooks "$hostOffset" addArchInfo
+      EOF
+    '';
+  }
