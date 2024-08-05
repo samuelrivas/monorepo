4740699cd2b6f396b162f2336ddbaac2f09ffca8
Samuel Rivas <samuelrivas@gmail.com>
2024-08-05 08:58:44 +0200

    wrap advent 2019

--- src/puzzles/adventofcode/2019/nix/default.nix
+++ src/puzzles/adventofcode/2019/nix/default.nix
@@ -5,31 +5,42 @@
   generic-lens,
   haskell-pkg,
   lens,
+  makeWrapper,
   monad-loops,
   readline,
   regex-pcre,
   searchlib,
   unliftio,
   unordered-containers,
-}:
-haskell-pkg {
-  name = "adventofcode-2019";
-  src = ./../src;
-  haskell-libs = [
-    adventlib-old-1
-    ansi-terminal
-    fingertree
-    generic-lens
-    lens
-    monad-loops
-    readline
-    regex-pcre
-    searchlib
-    unliftio
-    unordered-containers
-  ];
+}: let
+  advent-input-dir = ./../src/inputs;
+in
+  haskell-pkg {
+    name = "adventofcode-2019";
+    src = ./../src;
+    haskell-libs = [
+      adventlib-old-1
+      ansi-terminal
+      fingertree
+      generic-lens
+      lens
+      monad-loops
+      readline
+      regex-pcre
+      searchlib
+      unliftio
+      unordered-containers
+    ];
 
-  extra-drv = {
-    ADVENT_INPUT_DIR = ./../src/inputs;
-  };
-}
+    extra-build-inputs = [makeWrapper];
+
+    extra-drv = {
+      shellHook = ''
+        export ADVENT_INPUT_DIR=inputs
+      '';
+      postFixup = ''
+        wrapProgram "$out/bin/advent-2019" \
+        --set ADVENT_INPUT_DIR "${advent-input-dir}"
+      '';
+    };
+  }
