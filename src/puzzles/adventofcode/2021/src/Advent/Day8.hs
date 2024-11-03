e2c347d966b0413bd755d75065be111ea48dae04
Samuel Rivas <samuelrivas@gmail.com>
2024-11-03 10:48:23 +0100

    almost there

--- src/puzzles/adventofcode/2021/src/Advent/Day8.hs
+++ src/puzzles/adventofcode/2021/src/Advent/Day8.hs
@@ -11,7 +11,8 @@ module Advent.Day8 where
 import           Perlude
 
 import           Advent.Templib                (linesOf)
-import           Control.Lens                  (_2, at, over, to, toListOf)
+import           Control.Lens                  (_2, at, filtered, over, to,
+                                                toListOf)
 import           Data.Advent                   (Day (..))
 import           Data.Functor                  (($>))
 import           Data.Generics.Labels          ()
@@ -46,7 +47,10 @@ rawInput = getInput day
 
 example :: Text
 example = intercalate "\n"
-  ["be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+  [
+
+   "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf",
+   "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
    "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
    "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
    "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
@@ -95,15 +99,25 @@ constrainOutput w ws = over (at w) (fmap $ HashSet.intersection ws)
 negateWires :: HashSet Wire -> HashSet Wire
 negateWires = HashSet.difference allWires
 
--- Return the inputs that have been matched to a single output
-solvedInputs :: PossibleConnections -> [Wire]
-solvedInputs = HashMap.keys . HashMap.filter ((== 1) . HashSet.size)
+-- -- Return the outputs that are matched to a single input
+-- solvedOutputs :: PossibleConnections -> [Wire]
+-- solvedOutputs = HashMap.keys . HashMap.filter ((== 1) . HashSet.size)
+
+-- Return the inputs that are assigned to an output
+--
+-- TODO rewrite this as a single optics application, the bind operation is
+-- rather ugly here
+solvedInputs :: PossibleConnections -> HashSet Wire
+solvedInputs =
+  HashSet.unions . toListOf (traverse . filtered ((== 1) . HashSet.size))
+
+
 
 -- Given a set of inputs, get the outputs that can be constrained by that input
 -- set and the outputs that can be constrained as the negation of that same
 -- input set.
-constrainSet :: HashSet Wire -> ([Wire], [Wire])
-constrainSet inputs =
+getConstrainedOutputs :: HashSet Wire -> ([Wire], [Wire])
+getConstrainedOutputs inputs =
   case HashSet.size inputs of
     6 -> ([A, B, F, G], []) -- 0, 6, 9
     2 -> ([C, F], [A, B, D, E, G]) -- 1
@@ -112,6 +126,25 @@ constrainSet inputs =
     3 -> ([A, C, F], [B, D, E, G]) -- 7
     7 -> ([A .. G], []) -- 8
     _ -> error $ "invalid input set:" <> show inputs
+
+-- TODO: Make this prettier
+--
+-- FIXME: remove solved iputns the wires that are set from possible connections
+-- on other outputs
+constrainSolution :: PossibleConnections -> HashSet Wire -> PossibleConnections
+constrainSolution pcs inputs =
+  let
+    (positiveOutputs, negativeOutputs) = getConstrainedOutputs inputs
+    negatedInputs = negateWires inputs
+    updateConnections pcs' positiveOutput = constrainOutput positiveOutput inputs pcs'
+    updateNegConnections pcs' negOutput = constrainOutput negOutput negatedInputs pcs'
+    update1 = foldl updateConnections pcs positiveOutputs
+    update2 = foldl updateNegConnections update1 negativeOutputs
+      in
+    update2
+
+constrain :: PossibleConnections -> [HashSet Wire] -> PossibleConnections
+constrain = foldl constrainSolution
 
 -- use filtered lens to shorten this
 solver1 :: Parsed -> Int
