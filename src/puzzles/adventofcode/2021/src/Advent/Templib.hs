8c7c1d5cc971b60283d6b84d4ddc703f38a2f455
Samuel Rivas <samuelrivas@gmail.com>
2024-11-10 09:34:39 +0100

    use functions from Adventlib

--- src/puzzles/adventofcode/2021/src/Advent/Templib.hs
+++ src/puzzles/adventofcode/2021/src/Advent/Templib.hs
@@ -10,9 +10,7 @@
 {-# LANGUAGE UndecidableInstances  #-}
 
 module Advent.Templib (
-  binToDec,
   conv,
-  numListToNum,
   solveM,
   linesOf,
   bitString
@@ -23,7 +21,6 @@ import           Perlude
 import           Control.Monad.MonadEmit (EmitTVarT',
                                           runEmitTVarTWithPrinterThread')
 import           Data.Advent             (Day)
-import           Data.Foldable           (foldl')
 import           Data.Generics.Labels    ()
 import           Data.List               (tails)
 import           System.IO.Advent        (getParsedInput)
@@ -33,12 +30,6 @@ import           UnliftIO                (MonadUnliftIO)
 -- TODO use this in day 2 or delete
 conv :: ([a] -> b) -> [a] -> [b]
 conv f l = f <$> tails l
-
-binToDec :: [Bool] -> Int
-binToDec = foldl' (\acc b -> fromEnum b + 2*acc) 0
-
-numListToNum :: Num x => [x] -> x
-numListToNum = foldl (\acc x -> acc * 10 + x) 0
 
 -- Advent templates
 solveM ::
