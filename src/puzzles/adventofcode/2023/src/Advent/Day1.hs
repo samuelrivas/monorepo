c7b75c7e3625111ca85c4662736c2ec2cb16b993
Samuel Rivas <samuelrivas@gmail.com>
2024-11-13 19:48:47 +0100

    solved, ugly code

--- src/puzzles/adventofcode/2023/src/Advent/Day1.hs
+++ src/puzzles/adventofcode/2023/src/Advent/Day1.hs
@@ -18,7 +18,8 @@ import           Data.Maybe           (catMaybes, fromJust)
 import           Data.Num.Advent      (numListToDec)
 import           Data.Text            as Text
 import           System.IO.Advent     (getInput, getParsedInput)
-import           Text.Parsec          (many1, oneOf, sepEndBy, try)
+import           Text.Parsec          (anyChar, char, lookAhead, many1, oneOf,
+                                       sepEndBy, try)
 import           Text.Parsec.Char     (noneOf)
 import           Text.Parsec.Parselib (Parser, linesOf, literal, text,
                                        unsafeParseAll)
@@ -51,14 +52,16 @@ example2 = intercalate "\n" [
   "zoneight234",
   "7pqrstsixteen"
   ]
+
+-- This doesn't show on the examples, but textual numbers can overlap
+exampleOverlap :: Text
+exampleOverlap = "jjhxddmg5mqxqbgfivextlcpnvtwothreetwonerzk"
+
 parsedExample :: Parsed
 parsedExample = fromJust $ unsafeParseAll parser example
 
 parsedInput :: IO Parsed
 parsedInput = getParsedInput day parser
-
--- parser :: Parser Parsed
--- parser = linesOf $ text (noneOf ['\n'])
 
 parser :: Parser [[Digit]]
 parser = linesOf $ catMaybes <$> many1 maybeDigit
@@ -71,17 +74,20 @@ maybeDigit =
   Just <$> digit
   <|> noneOf  ['\n'] $> Nothing
 
+consumeOne :: Text -> Parser Text
+consumeOne t = (lookAhead . literal $ t) >> anyChar $> t
+
 digit :: Parser Digit
 digit =
-  try (literal "one") $> Textual 1
-  <|> try (literal "two") $> Textual 2
-  <|> try (literal "three") $> Textual 3
-  <|> try (literal "four") $> Textual 4
-  <|> try (literal "five") $> Textual 5
-  <|> try (literal "six") $> Textual 6
-  <|> try (literal "seven") $> Textual 7
-  <|> try (literal "eight") $> Textual 8
-  <|> try (literal "nine") $> Textual 9
+  try (consumeOne "one") $> Textual 1
+  <|> try (consumeOne "two") $> Textual 2
+  <|> try (consumeOne "three") $> Textual 3
+  <|> try (consumeOne "four") $> Textual 4
+  <|> try (consumeOne "five") $> Textual 5
+  <|> try (consumeOne "six") $> Textual 6
+  <|> try (consumeOne "seven") $> Textual 7
+  <|> try (consumeOne "eight") $> Textual 8
+  <|> try (consumeOne "nine") $> Textual 9
   <|> Literal . digitToInt <$> try (oneOf ['0'..'9'])
 
 firstLiteral :: [Digit] -> Int
@@ -95,12 +101,11 @@ toInt (Textual x) = x
 
 calibrationValue :: [Digit] -> Int
 calibrationValue digits =
-  10 * (firstLiteral digits) + firstLiteral (Perlude.reverse digits)
+  10 * firstLiteral digits + firstLiteral (Perlude.reverse digits)
 
 calibrationValue2 :: [Digit] -> Int
 calibrationValue2 digits =
-  10 * (toInt $ Perlude.head digits) + (toInt . Perlude.last $ digits)
-
+  10 * toInt (Perlude.head digits) + (toInt . Perlude.last $ digits)
 
 solve1 :: Parsed -> Int
 solve1 = sum . fmap calibrationValue
