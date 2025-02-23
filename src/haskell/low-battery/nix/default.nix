c4fa39ebe013782007bdf5190d9ea6224d923378
Samuel Rivas <samuelrivas@gmail.com>
2025-02-23 07:38:59 +0100

    fix sandbox for low-battery

--- src/haskell/low-battery/nix/default.nix
+++ src/haskell/low-battery/nix/default.nix
@@ -1,13 +1,13 @@
 {
+  HSH,
   acpi,
+  generic-lens,
   haskell-pkg,
+  lens,
   lib,
+  parsec,
   pkgs, # we get libnotify from here, if we write libnotify directly we get a haskell library
   writeShellScriptBin,
-  HSH,
-  parsec,
-  generic-lens,
-  lens,
 }: let
   binary = haskell-pkg {
     name = "low-battery";
@@ -18,6 +18,7 @@
       generic-lens
       lens
     ];
+    extra-native-build-inputs = [acpi];
     extra-drv = {meta.platforms = lib.platforms.linux;};
   };
   # FIXME The DBUS address should not be hardcoded...
@@ -26,8 +27,11 @@
     export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
     ${binary}/bin/low-battery-check
   '';
-  # output binary instead of script if you want to sandbox this
-  # TODO fix this guacamole
-  # in binary
 in
-  script // {meta.platforms = lib.platforms.linux;}
+  ## We use the haskell binary dev shell, the script is easy to debug just by
+  ## building the package
+  script
+  // {
+    passthru.dev-shell = binary.passthru.dev-shell;
+    meta.platforms = lib.platforms.linux;
+  }
