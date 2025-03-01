37df50ea7b34b8aa9871477a8151cf87c855a89b
Samuel Rivas <samuelrivas@gmail.com>
2025-03-01 11:14:55 +0100

    Build the binary directly, rather than embedding it in a shell script.
    Especially when the script encodes local information. We can now use this binary
    to set up a task in cron or a systemd timer.

--- src/haskell/low-battery/nix/default.nix
+++ src/haskell/low-battery/nix/default.nix
@@ -3,30 +3,25 @@
   haskell-pkg,
   lib,
   libnotify,
+  makeWrapper,
   perlude,
-  writeShellScriptBin,
-}: let
-  binary = haskell-pkg {
-    name = "low-battery";
-    src = ./../src;
-    haskell-libs = [
-      HSH
-      perlude
-    ];
-    extra-native-build-inputs = [libnotify];
-    extra-drv = {meta.platforms = lib.platforms.linux;};
+}:
+haskell-pkg {
+  name = "low-battery";
+  src = ./../src;
+  haskell-libs = [
+    HSH
+    perlude
+  ];
+  extra-native-build-inputs = [libnotify];
+  extra-drv = {meta.platforms = lib.platforms.linux;};
+
+  extra-build-inputs = [makeWrapper];
+
+  extra-drv = {
+    postFixup = ''
+      wrapProgram "$out/bin/low-battery-check" \
+        --prefix PATH : ${libnotify}/bin
+    '';
   };
-  # FIXME The DBUS address should not be hardcoded...
-  script = writeShellScriptBin "low-battery-notify" ''
-    export PATH=${libnotify}/bin
-    export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1001/bus
-    ${binary}/bin/low-battery-check
-  '';
-in
-  ## We use the haskell binary dev shell, the script is easy to debug just by
-  ## building the package
-  script
-  // {
-    passthru.dev-shell = binary.passthru.dev-shell;
-    meta.platforms = lib.platforms.linux;
-  }
+}
