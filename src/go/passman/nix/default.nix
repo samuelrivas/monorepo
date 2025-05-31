8d3bb72342e02b37f54e68269b7a7895e791a19d
Samuel Rivas <samuelrivas@gmail.com>
2025-05-31 06:37:04 +0200

    getting a libray in, but we still don't use it

--- src/go/passman/nix/default.nix
+++ src/go/passman/nix/default.nix
@@ -1,14 +1,9 @@
-{
-  buildGoModule,
-  go,
-}:
+{buildGoModule}:
 buildGoModule {
   pname = "passman-go";
   version = "1.0.0";
 
   src = ../src/src;
 
-  vendorHash = null;
-
-  nativeBuildInputs = [go];
+  vendorHash = "sha256-wQj/uLY7NrG/t6/pd/Fnm0tmuRKjK46TKoJVETB5ed0=";
 }
