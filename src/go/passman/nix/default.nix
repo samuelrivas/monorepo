a49f07cb3fb3a883bc384ad4846e63f961bc4fac
Samuel Rivas <samuelrivas@gmail.com>
2025-06-07 07:33:12 +0200

    better go dev shell

--- src/go/passman/nix/default.nix
+++ src/go/passman/nix/default.nix
@@ -1,9 +1,32 @@
-{buildGoModule}:
-buildGoModule {
-  pname = "passman-go";
-  version = "1.0.0";
+{
+  add-dev-shell,
+  buildGoModule,
+}: let
+  drv = buildGoModule {
+    pname = "passman-go";
+    version = "1.0.0";
 
-  src = ../src/src;
+    src = ../src/src;
 
-  vendorHash = "sha256-wQj/uLY7NrG/t6/pd/Fnm0tmuRKjK46TKoJVETB5ed0=";
-}
+    vendorHash = "sha256-jwQvAIS8XCWjZtP9pKt1RRSaKBZ9dgDc2SD6q0K+sEs=";
+  };
+
+  # This could be extracted if we had more go projects in this repo
+  #
+  # When in the sandbox, we use go mod tools to get the dependencies, and set
+  # directories in /tmp as cache. When building, nix will use vendoring and
+  # check the downloaded files against vendorHash.
+  extra-sandbox = {
+    shell-hook = ''
+      # We could use $TMPDIR if we wanted to make these specific to the sandbox,
+      # but that requires us to download everything again for every new sandbox we
+      # create
+      export GOCACHE=/tmp/go-sandbox/cache
+      export GOMODCACHE=/tmp/go-sandbox/modcache
+
+      # Disable vendoring while in the sandbox
+      GOFLAGS="$GOFLAGS -mod=mod"
+    '';
+  };
+in
+  add-dev-shell drv extra-sandbox
