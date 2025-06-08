3a65b1e151f061d58511908f9e3f15178aef5318
Samuel Rivas <samuelrivas@gmail.com>
2025-06-08 07:20:39 +0200

    The PoC now reads a password and can decrypt an age encrypted file.

--- src/go/passman/src/src/passman.go
+++ src/go/passman/src/src/passman.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
-	"github.com/ProtonMail/go-crypto/openpgp"
+
+	"filippo.io/age"
+	"golang.org/x/term"
 )
-
-type Abser interface {
-	Abs() float64
-}
-
 
 func parseArgs(args []string) (string, error) {
 	if len(args) < 2 {
@@ -34,18 +32,45 @@ func usage(args []string) {
 	log("Usage: %s <filename>\n\n", filepath.Base(args[0]))
 }
 
+func askForPassword() string {
+	fmt.Fprint(os.Stderr, "Enter password: ")
+	password, err := term.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		logLn("Error reading password")
+		panic(err)
+	}
+	fmt.Fprintln(os.Stderr)
+	return string(password)
+}
+
 func main() {
 	args := os.Args
 	fileName, err := parseArgs(args)
 	if err != nil {
 		usage(args)
-		logLn("Error: %v", err)
-		return
+		panic(err)
 	}
-	content, err := os.ReadFile(fileName)
+
+	fd, err := os.Open(fileName)
 	if err != nil {
-		logLn("Error reading file: %v", err)
-		return
+		logLn("Error opening file")
+		panic(err)
 	}
-	fmt.Println("File content:", string(content))
+	defer fd.Close()
+
+	password:= askForPassword()
+
+	identity, err := age.NewScryptIdentity(string(password))
+	if err != nil {
+		logLn("Error creating identity")
+		panic(err)
+	}
+
+	clear, err := age.Decrypt(fd, identity)
+	if err != nil {
+		logLn("Error decrypting file")
+		panic(err)
+	}
+
+	io.Copy(os.Stdout, clear)
 }
