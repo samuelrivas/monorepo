0a79cac86ea3fa814c7e643e62bf83bfabe8a4d1
Samuel Rivas <samuelrivas@gmail.com>
2025-07-03 08:26:28 +0200

    some testing draft

---
+++ src/go/passman/src/src/test/encrypt.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# This script requires next environmet variables to be set
+#  * SH_LIB: the lib directory to load utils from
+
+source $SH_LIB/prelude.sh
+
+if [[ "$NARGS" != 1 ]]; then
+    echo "uasge: $PROGNAME>"
+fi
+
+
+# use script to inject the password with a pseudo terminal
+
+cd "$PROGDIR"
+echo "Use foobar as passphrase"
+age --encrypt --passphrase < testfile.sexp > testfile.age
