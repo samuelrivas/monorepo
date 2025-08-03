5728d51cae41e26d1f05f834de7aa946e774fcf4
Samuel Rivas <samuelrivas@gmail.com>
2025-08-03 17:14:12 +0200

    simplify a bit

--- nix/pkgs/applications/networking/tools/kattis/nix/default.nix
+++ nix/pkgs/applications/networking/tools/kattis/nix/default.nix
@@ -10,9 +10,7 @@
     sha256 = "sha256-6m/ICA4X8lLec7K95vv6vv/y5e5Hsxku+qVmOoDFMBw=";
   };
   python = python3Packages.python.withPackages (ps: with ps; [requests lxml]);
-
-  script = writeShellScriptBin "kattis" ''
-    exec ${python}/bin/python ${src}/submit.py "$@"
-  '';
 in
-  script
+  writeShellScriptBin "kattis-submit" ''
+    exec ${python}/bin/python ${src}/submit.py "$@"
+  ''
