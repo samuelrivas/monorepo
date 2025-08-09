3bf68fb9fa060ab3ef31727f6d030749703beb4f
Samuel Rivas <samuelrivas@gmail.com>
2025-08-09 22:47:41 +0200

    solve reduplikation

--- src/puzzles/kattis/nix/default.nix
+++ src/puzzles/kattis/nix/default.nix
@@ -1,12 +1,14 @@
 {
   haskell-pkg,
   kattis-cli,
+  parselib,
   perlude,
 }: let
   drv = haskell-pkg {
     name = "kattis-haskell";
     src = ./../src;
     haskell-libs = [
+      parselib
       perlude
     ];
     extra-shell-native-build-inputs = [kattis-cli];
---
+++ src/puzzles/kattis/src/src/Reduplikation.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Reduplikation where
+
+import           Control.Monad        (replicateM_)
+import           Data.Text            (intercalate)
+import           Perlude
+import           Text.Parsec          (newline, noneOf)
+import           Text.Parsec.Parselib (Parser, digitsAsNum, text1, unsafeParse)
+
+example :: Text
+example =
+  intercalate
+    "\n"
+    [ "hej",
+      "3"
+    ]
+
+parser :: Parser (Text, Int)
+parser =
+  (,)
+    <$> text1 (noneOf ['\n'])
+    <* newline
+    <*> digitsAsNum
+
+main :: IO ()
+main = do
+  (text, reps) <- getContents >>= unsafeParse parser
+  replicateM_ reps (putStr text)
