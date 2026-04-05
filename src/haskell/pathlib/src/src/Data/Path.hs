851cfbf8104dc4ee4ee4c061b09c1bfa62c6ad04
Samuel Rivas <samuelrivas@gmail.com>
2026-04-05 20:43:13 +0200

    Add functions to convert back and forth between Text and Path

--- src/haskell/pathlib/src/src/Data/Path.hs
+++ src/haskell/pathlib/src/src/Data/Path.hs
@@ -7,11 +7,13 @@ module Data.Path where
 
 import           Perlude
 
+import Data.List (intersperse)
 import           GHC.Base             ((<|>))
 import           Text.Parsec          (char, many, many1, noneOf)
 import           Text.Parsec.Parselib (Parser, text1, unsafeParseAll)
-import Data.Maybe (fromJust, listToMaybe, fromMaybe)
+import Data.Maybe (fromJust, listToMaybe, fromMaybe, catMaybes)
 import GHC.Stack (HasCallStack)
+import qualified Data.Text as T
 
 data Token = Slash | Component Text
   deriving stock (Show, Eq)
@@ -36,3 +38,23 @@ isAbsolute = fromMaybe False . fmap (== Slash) . listToMaybe . unPath
 
 isRelative :: Path -> Bool
 isRelative = not . isAbsolute
+
+components :: Path -> [Text]
+components =
+  let
+    toText Slash = Nothing
+    toText (Component a) = Just a
+  in
+    catMaybes . fmap toText . unPath
+
+fromComponents :: Bool -> [Text] -> Path
+fromComponents True = Path . (Slash :) . intersperse Slash . fmap Component
+fromComponents False =  Path  . intersperse Slash . fmap Component
+
+toText :: Path -> Text
+toText =
+  let
+    tt Slash = "/"
+    tt (Component a) = a
+  in
+    T.concat . fmap tt . unPath
