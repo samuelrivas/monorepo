7eb4875de51566720c9a146849f87a025f3c453a
Samuel Rivas <samuelrivas@gmail.com>
2026-04-05 09:10:53 +0200

    Implement fromText, isAbsolute and isRelative

--- src/haskell/pathlib/src/src/Data/Path.hs
+++ src/haskell/pathlib/src/src/Data/Path.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE NoImplicitPrelude  #-}
 {-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE DerivingVia #-}
 
 module Data.Path where
 
@@ -8,10 +9,15 @@ import           Perlude
 
 import           GHC.Base             ((<|>))
 import           Text.Parsec          (char, many, many1, noneOf)
-import           Text.Parsec.Parselib (Parser, text1)
+import           Text.Parsec.Parselib (Parser, text1, unsafeParseAll)
+import Data.Maybe (fromJust, listToMaybe, fromMaybe)
+import GHC.Stack (HasCallStack)
 
 data Token = Slash | Component Text
-  deriving stock Show
+  deriving stock (Show, Eq)
+
+newtype Path = Path { unPath :: [Token] }
+  deriving Show via [Token]
 
 path :: Parser [Token]
 path = many (component <|> slash)
@@ -21,3 +27,12 @@ slash = (many1 . char $ '/') *> pure Slash
 
 component :: Parser Token
 component = Component <$> text1 (noneOf "/")
+
+fromText :: HasCallStack => Text -> Path
+fromText = Path . fromJust . unsafeParseAll path
+
+isAbsolute :: Path -> Bool
+isAbsolute = fromMaybe False . fmap (== Slash) . listToMaybe . unPath
+
+isRelative :: Path -> Bool
+isRelative = not . isAbsolute
